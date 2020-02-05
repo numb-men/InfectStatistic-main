398c0215f81ec277f2ea5459927485adaf40ceb4
herokilito <herokilito@outlook.com>
2020-02-05 19:04:47 +0800

    初始化项目和编写codestyle

---
+++ 221701328/src/InfectStatistic.java
@@ -0,0 +1,13 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author herokilito@outlook.com
+ * @version 0.1
+ * @since 2020.02
+ */
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ 221701328/src/Lib.java
@@ -0,0 +1,10 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author herokilito@outlook.com
+ * @version 0.1
+ * @since 2020.02
+ */
+public class Lib {
+}
