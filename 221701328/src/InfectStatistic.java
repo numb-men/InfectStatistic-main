b274cc17f18dcb364b18c670efbf93713f9a96a4
herokilito <herokilito@outlook.com>
2020-02-06 12:16:36 +0800

    实现了参数分离的功能

--- 221701328/src/InfectStatistic.java
+++ 221701328/src/InfectStatistic.java
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -6,7 +9,7 @@ import java.util.TreeMap;
  * TODO
  *
  * @author herokilito@outlook.com
- * @version 0.2
+ * @version 0.3
  * @since 2020.02
  */
 class InfectStatistic {
@@ -31,21 +34,23 @@ class InfectStatistic {
     private String logParam;
     private String outParam;
     private String dateParam;
-    private String[] typeParams;
-    private String[] provinceParams;
+    private ArrayList<String> typeParams;
+    private ArrayList<String> provinceParams;
 
     /*用于保存各省份疫情信息*/
     private Map<String, Map<String,Integer>> statistics;
 
     /*构造方法*/
     public InfectStatistic(){
-        hasLog=false;
-        hasOut=false;
-        hasDate=false;
-        hasType=false;
-        hasProvince=false;
-        statistics=new TreeMap<>();
-        Map<String,Integer> data=new TreeMap<>(); //全国数据
+        hasLog = false;
+        hasOut = false;
+        hasDate = false;
+        hasType = false;
+        hasProvince = false;
+        typeParams = new ArrayList<>();
+        provinceParams = new ArrayList<>();
+        statistics = new TreeMap<>();
+        Map<String,Integer> data = new TreeMap<>(); //全国数据
         data.put(INFECTION_PATIENT,0);
         data.put(SUSPECTED_PATIENT,0);
         data.put(CURE,0);
@@ -54,7 +59,7 @@ class InfectStatistic {
     }
 
     public static void main(String[] args) {
-        InfectStatistic infectStatistic=new InfectStatistic();
+        InfectStatistic infectStatistic = new InfectStatistic();
         infectStatistic.execute(args);
     }
 
@@ -63,7 +68,87 @@ class InfectStatistic {
      * @param args 传递给main方法的参数
      */
     public void execute(String[] args) {
-
+        if(args.length == 0){
+            Lib.help();    //显示提示信息
+            System.exit(0);
+        }
+        /*分离参数*/
+        switch (args[0]){
+            case "list":
+                int i = 1;
+                while(i < args.length){
+                    switch (args[i]){
+                        case "-log":
+                            hasLog = true;
+                            if(++i >= args.length){
+                                System.out.println("log参数缺少参数值");
+                                System.exit(1);
+                            }
+                            logParam = args[i++];
+                            break;
+                        case "-out":
+                            hasOut = true;
+                            if(++i >= args.length){
+                                System.out.println("out参数缺少参数值");
+                                System.exit(1);
+                            }
+                            outParam = args[i++];
+                            break;
+                        case "-date":
+                            hasDate = true;
+                            if(++i >= args.length){
+                                System.out.println("date参数缺少参数值");
+                                System.exit(1);
+                            }
+                            dateParam = args[i++];
+                            break;
+                        case  "-type":
+                            hasType = true;
+                            while(++i < args.length && !args[i].equals("-log") && !args[i].equals("-out") && !args[i].equals("-date")
+                                    && !args[i].equals("-province")){
+                                typeParams.add(args[i]);
+                            }
+                            break;
+                        case  "-province":
+                            hasProvince = true;
+                            while(++i < args.length && !args[i].equals("-log") && !args[i].equals("-out") && !args[i].equals("-date")
+                                    && !args[i].equals("-type")){
+                                provinceParams.add(args[i]);
+                            }
+                            break;
+                        default:
+                            System.out.println("\""+args[i]+"\"无法解析的参数");
+                            System.exit(1);
+                            break;
+                    }
+                }
+                break;
+            //case something:此处可扩展其他命令
+            default:
+                System.out.println("无效的命令");
+                System.exit(1);
+        }
+        /*执行相应的方法*/
+        if(!hasLog){  //log必须有
+            System.out.println("缺少-log参数");
+            System.exit(1);
+        }
+        if(!hasOut){  //out必须有
+            System.out.println("缺少-out参数");
+            System.exit(1);
+        }
+        if(!hasDate){  //如果没有data参数
+            dateParam=new SimpleDateFormat("yyyy-MM-dd").format(new Date()); //当前日期
+        }
+        doLog(logParam);    //读取日志路径
+        doDate(dateParam);   //读取日志路径下相应日期的日志
+        if(hasType){
+            doType(typeParams);   //需要输出的信息类型
+        }
+        if(hasProvince){
+            doProvince(provinceParams);   //需要输出的省份疫情信息
+        }
+        doOut(outParam);  //输出到指定的路径
     }
 
     /**
@@ -71,7 +156,7 @@ class InfectStatistic {
      * @param logPath -log参数后面的log文件路径
      */
     private void doLog(String logPath){
-
+        System.out.println("log:"+logPath);
     }
 
     /**
@@ -79,7 +164,7 @@ class InfectStatistic {
      * @param outPath -out参数后面的输出路径
      */
     private void doOut(String outPath){
-
+        System.out.println("out:"+outPath);
     }
 
     /**
@@ -87,22 +172,30 @@ class InfectStatistic {
      * @param date -date参数后面的具体日期
      */
     private void doDate(String date){
-
+        System.out.println("date:"+date);
     }
 
     /**
      *执行-type命令参数
      * @param types -type命令参数后面的具体参数值数组
      */
-    private void doType(String types){
-
+    private void doType(ArrayList<String> types){
+        System.out.print("types:");
+        for(String s:types){
+            System.out.print(s+" ");
+        }
+        System.out.println("");
     }
 
     /**
      *执行-province命令参数
      * @param provinces -province命令参数后面的具体参数值数组
      */
-    private void doProvince(String provinces){
-
+    private void doProvince(ArrayList<String> provinces){
+        System.out.print("provinces:");
+        for(String s:provinces){
+            System.out.print(s+" ");
+        }
+        System.out.println("");
     }
 }
--- 221701328/src/Lib.java
+++ 221701328/src/Lib.java
@@ -1,10 +1,15 @@
 /**
  * InfectStatistic
  * TODO
+ *程序执行过程中需要的一些静态方法
  *
  * @author herokilito@outlook.com
- * @version 0.1
+ * @version 0.3
  * @since 2020.02
  */
 public class Lib {
+
+    public static void help(){   //help message in here
+        System.out.println("");
+    }
 }
