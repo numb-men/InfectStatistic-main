73b5ffe68107c3e6c453e190cb93a01235a3b38f
theguiltyperson <1023635049@qq.com>
2020-02-08 01:21:53 +0800

    解析命令
    用HashMap解析命令，把命令参数和参数值以键值对的方式存储。

--- 221701429/src/InfectStatistic.java
+++ 221701429/src/InfectStatistic.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * InfectStatistic
  * TODO
@@ -7,7 +10,39 @@
  * @since xxx
  */
 class InfectStatistic {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
+    //建立HashMap，把命令参数和参数值分开
+    private static HashMap<String, String> parseArgs(String[] args) {
+        HashMap<String, String> tmp = new HashMap<String, String>();
+        String key = null;
+        //判断命令是否为list
+        if (!args[0].equals("list")) {
+            System.out.println("该命令不存在！");
+            return tmp;
+        }
+        //命令参数和参数值放入HashMap中
+        for (int i = 1; i<args.length; i++){
+            String arg = args[i];
+            if (arg.startsWith("-")){
+                if (key == null){
+                    key = arg;
+                }
+                else{
+                    tmp.put(key,null);
+                    key = arg;
+                }
+            }
+            else {
+                tmp.put(key, arg);
+                key = null;
+            }
+        }
+       return tmp;
+    }
+
+    public static void main(String args[]) {
+        String cmdLine = "list -date -log D:/log/ -out D:/output.txt";
+        args = cmdLine.split(" ");
+        HashMap<String, String> parseArgs = parseArgs(args);
+        System.out.println(parseArgs);
     }
 }
