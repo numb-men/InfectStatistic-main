b5af107199f8fbf86ece89ffc0aa52bd48914a88
theguiltyperson <1023635049@qq.com>
2020-02-08 02:56:49 +0800

    读取目录下文件
    读取目录下txxt文件的内容

--- 221701429/src/InfectStatistic.java
+++ 221701429/src/InfectStatistic.java
@@ -1,5 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * InfectStatistic
@@ -38,11 +42,84 @@ class InfectStatistic {
         }
        return tmp;
     }
+    //命令对应的行为
+    private static void func(HashMap<String, String> parseArgs){
+        if(parseArgs.containsKey("-log")){
+            String path = parseArgs.get("-log");
+            readFile(path);//读取文件夹下的文件
+        }
+        else {
+            System.out.println("必须附带log参数！");
+            return;
+        }
+        if(parseArgs.containsKey("-out")){
 
-    public static void main(String args[]) {
-        String cmdLine = "list -date -log D:/log/ -out D:/output.txt";
+        }
+        else {
+            System.out.println("必须附带out参数！");
+            return;
+        }
+        if(parseArgs.containsKey("-date")){
+
+        }
+        if(parseArgs.containsKey("-type")){
+            String type = parseArgs.get("-type");
+            switch (type){
+                case "ip":break;
+                case "sp":break;
+                case "cure":break;
+                case "dead":break;
+                default:
+            }
+        }
+        if(parseArgs.containsKey("-province")){
+
+        }
+    }
+    //读取目录下的日志
+    public static void readFile(String path){
+        File all = new File(path);
+        List allPath = getFile(all);
+        for(int i = 0; i < allPath.size(); i++){
+            File file = new File(String.valueOf(allPath.get(i)));
+            String content = txt2String(file);
+            //System.out.println(content);
+        }
+    }
+    //txt转String
+    public static String txt2String(File file) {
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));// 构造一个BufferedReader类来读取文件
+            String s = null;
+            while ((s = br.readLine()) != null) {// 使用readLine方法，一次读一行
+                result.append(System.lineSeparator() + s);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+    // 读取文件夹下所有文件名
+    public static List getFile(File file) {
+        List listLocal = new ArrayList<>();
+        if (file != null) {
+            File[] f = file.listFiles();
+            if (f != null) {
+                for (int i = 0; i < f.length; i++) {
+                    getFile(f[i]);
+                    listLocal.add(f[i]);
+                }
+            }
+        }
+        return listLocal;
+    }
+    public static void main(String[] args) {
+        String cmdLine = "list -date -log C:/Users/ASUS/Documents/GitHub/InfectStatistic-main/221701429/log -out D:/output.txt";
         args = cmdLine.split(" ");
         HashMap<String, String> parseArgs = parseArgs(args);
         System.out.println(parseArgs);
+        func(parseArgs);
     }
 }
