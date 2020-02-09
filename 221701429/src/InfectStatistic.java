c87fb551ee6bc409ef3ea6537b67c9ce52de7eae
theguiltyperson <1023635049@qq.com>
2020-02-09 23:47:47 +0800

    实现了各省份人数的统计功能
    match函数把患者增减的8种情况分类计算进去，返回list，list里存储province类，可以用printResult函数输出l结果

--- 221701429/src/InfectStatistic.java
+++ 221701429/src/InfectStatistic.java
@@ -16,15 +16,11 @@ import java.util.regex.Pattern;
  */
 class InfectStatistic {
     static class province{
-        /*enum name{
-            北京,天津,上海,重庆,河北,山西,辽宁,吉林,黑龙江,江苏,浙江,安徽,福建,江西, 山东,河南,湖北,
-            湖南,广东,海南,四川,贵州,云南,陕西,甘肃,青海,台湾,内蒙古, 广西,西藏,宁夏,新疆,香港,澳门
-        }*/
-        String name;
-        int ip;//感染
-        int sp;//疑似
-        int cure;//治愈
-        int dead;//死亡
+        private String name;
+        private int ip;//感染
+        private int sp;//疑似
+        private int cure;//治愈
+        private int dead;//死亡
         province(String name, int ip, int sp, int cure, int dead){
             this.name = name;
             this.ip = ip;
@@ -32,6 +28,43 @@ class InfectStatistic {
             this.cure = cure;
             this.dead = dead;
         }
+        public String getName(){
+            return name;
+        }
+        public int getIp(){
+            return ip;
+        }
+        public int getSp(){
+            return sp;
+        }
+        public int getCure(){
+            return cure;
+        }
+        public int getDead() {
+            return dead;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setIp(int ip) {
+            this.ip = ip;
+        }
+        public void setSp(int sp) {
+            this.sp = sp;
+        }
+        public void setCure(int cure) {
+            this.cure = cure;
+        }
+        public void setDead(int dead) {
+            this.dead = dead;
+        }
+        public String printResult(){
+            return name+" 感染患者"+ip+"人"+" 疑似患者"+sp+"人"+ " 治愈"+cure+"人"+" 死亡"+dead+"人";
+        }
+    }
+    public static province init(){//初始化province类
+        province pro = new province(null,0,0,0,0);
+        return pro;
     }
     //建立HashMap，把命令参数和参数值分开
     private static HashMap<String, String> parseArgs(String[] args) {
@@ -66,7 +99,7 @@ class InfectStatistic {
         if (parseArgs.containsKey("-log")){
             String path = parseArgs.get("-log");
             String[] allContent = readFile(path);//读取文件夹下的文件
-            String[] result = match(allContent);//对内容分析返回结果
+            ArrayList<province> list = match(allContent);//对内容分析返回结果
 
         }
         else {
@@ -98,14 +131,24 @@ class InfectStatistic {
         }
     }
 
+    //判断list中是否含有指定name属性的province
+    private static boolean isListName(ArrayList<province> list, String name){
+        for(int i = 0; i < list.size(); i++){
+            if (list.get(i).getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
     //对读取到的内容正则匹配
-    private static String[] match(String[] allContent){
-        String[] result = new String[allContent.length];
+    private static ArrayList<province> match(String[] allContent){
+        //String[] result = new String[allContent.length];
+        ArrayList<province> list = new ArrayList<province>();
         try{
+
             for(int i = 0; i<allContent.length; i++){
                 BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(allContent[i].getBytes())));
                 String s;
-                ArrayList<String> list = new ArrayList<String>();
                 while((s = br.readLine()) != null){
                     Pattern p1 = Pattern.compile("(.*) 新增 感染患者 (\\d*)人");
                     Pattern p2 = Pattern.compile("(.*) 新增 疑似患者 (\\d*)人");
@@ -123,44 +166,130 @@ class InfectStatistic {
                     Matcher m6 = p6.matcher(s);
                     Matcher m7 = p7.matcher(s);
                     Matcher m8 = p8.matcher(s);
-                    while(m1.find()){
-                        System.out.println(m1.group(1)+m1.group(2));
+                    while(m1.find()){//新增感染患者
+                        if(!isListName(list, m1.group(1))){//若该省份不存在则新建
+                            province pro = new province(m1.group(1),Integer.parseInt(m1.group(2)),0,0,0);
+                            list.add(pro);
+                        }
+                        else{//存在该省份，则修改ip
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m1.group(1))){
+                                    list.get(j).setIp(Integer.parseInt(m1.group(2)) + list.get(j).getIp());//修改该省份的感染患者人数
+                                }
+                            }
+                        }
                     }
-                    while(m2.find()){
-                        System.out.println(m2.group(1)+m2.group(2));
+                    while(m2.find()){//新增疑似患者
+                        if(!isListName(list, m2.group(1))){//若该省份不存在则新建
+                            province pro =new province(m2.group(1),0,Integer.parseInt(m2.group(2)),0,0);
+                            list.add(pro);
+                        }
+                        else{//存在该省份，则修改sp
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m2.group(1))){
+                                    list.get(j).setSp(Integer.parseInt(m2.group(2)) + list.get(j).getSp());//修改该省份的疑似患者人数
+                                }
+                            }
+                        }
                     }
-                    while(m3.find()){
-                        System.out.println(m3.group(1)+m3.group(2)+m3.group(3));
+                    while(m3.find()){//感染患者迁移
+                        if(!isListName(list, m3.group(1))){//流出省不存在则输出错误
+                            System.out.println("该省份不存在，无法流出！");
+                        }
+                        else if(!isListName(list, m3.group(2))) {//流入省不存在则新建
+                            province pro =new province(m3.group(2),Integer.parseInt(m3.group(3)),0,0,0);
+                            list.add(pro);
+                        }
+                        else{//流出流入省都存在，则修改两省的ip
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m3.group(1))){
+                                    list.get(j).setIp(list.get(j).getIp() - Integer.parseInt(m3.group(3)));//修改流出省的感染患者人数
+                                }
+                                if(list.get(j).getName().equals(m3.group(2))){
+                                    list.get(j).setIp(list.get(j).getIp() + Integer.parseInt(m3.group(3)));//修改流入省的感染患者人数
+                                }
+                            }
+                        }
                     }
-                    while(m4.find()){
-                        System.out.println(m4.group(1)+m4.group(2)+m4.group(3));
+                    while(m4.find()){//疑似患者迁移
+                        if(!isListName(list, m4.group(1))){//流出省不存在则输出错误
+                            System.out.println("该省份不存在，无法流出！");
+                        }
+                        else if(!isListName(list, m4.group(2))) {//流入省不存在则新建
+                            province pro =new province(m4.group(2),0,Integer.parseInt(m4.group(3)),0,0);
+                            list.add(pro);
+                        }
+                        else{//流出流入省都存在，则修改两省的sp
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m4.group(1))){
+                                    list.get(j).setSp(list.get(j).getSp() - Integer.parseInt(m4.group(3)));//修改流出省的疑似患者人数
+                                }
+                                if(list.get(j).getName().equals(m4.group(2))){
+                                    list.get(j).setSp(list.get(j).getSp() + Integer.parseInt(m4.group(3)));//修改流入省的疑似患者人数
+                                }
+                            }
+                        }
                     }
-                    while(m5.find()){
-                        System.out.println(m5.group(1)+m5.group(2));
+                    while(m5.find()){//死亡
+                        if(!isListName(list, m5.group(1))){//若该省份不存在则输出错误
+                            System.out.println("该省份无感染患者，无法死亡！");
+                        }
+                        else{//存在该省份，则修改ip,dead
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m5.group(1))){
+                                    list.get(j).setIp(list.get(j).getIp() - Integer.parseInt(m5.group(2)));//修改该省份的感染患者人数
+                                    list.get(j).setDead(Integer.parseInt(m5.group(2)) + list.get(j).getDead());//修改该省份的死亡人数
+                                }
+                            }
+                        }
                     }
-                    while(m6.find()){
-                        System.out.println(m6.group(1)+m6.group(2));
+                    while(m6.find()){//治愈
+                        if(!isListName(list, m6.group(1))){//若该省份不存在则输出错误
+                            System.out.println("该省份无感染患者，无法治愈！");
+                        }
+                        else{//存在该省份，则修改ip,cure
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m6.group(1))){
+                                    list.get(j).setIp(list.get(j).getIp() - Integer.parseInt(m6.group(2)));//修改该省份的感染患者人数
+                                    list.get(j).setCure(Integer.parseInt(m6.group(2)) + list.get(j).getCure());//修改该省份的治愈人数
+                                }
+                            }
+                        }
                     }
-                    while(m7.find()){
-                        System.out.println(m7.group(1)+m7.group(2));
+                    while(m7.find()){//疑似患者确诊感染
+                        if(!isListName(list, m7.group(1))){//若该省份不存在则输出错误
+                            System.out.println("该省份无疑似患者，无法确诊！");
+                        }
+                        else{//存在该省份，则修改ip,sp
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m7.group(1))){
+                                    list.get(j).setIp(Integer.parseInt(m7.group(2)) + list.get(j).getIp());//修改该省份的感染患者人数
+                                    list.get(j).setSp(list.get(j).getSp() - Integer.parseInt(m7.group(2)));//修改该省份的疑似患者人数
+                                }
+                            }
+                        }
                     }
-                    while(m8.find()){
-                        System.out.println(m8.group(1)+m8.group(2));
+                    while(m8.find()){//排除疑似患者
+                        if(!isListName(list, m8.group(1))){//若该省份不存在则输出错误
+                            System.out.println("该省份无疑似患者，无法排除！");
+                        }
+                        else{//存在该省份，则修改sp
+                            for(int j = 0; j < list.size(); j++){
+                                if(list.get(j).getName().equals(m8.group(1))){
+                                    list.get(j).setSp(list.get(j).getSp() - Integer.parseInt(m8.group(2)));//修改该省份的疑似患者人数
+                                }
+                            }
+                        }
                     }
+                }
             }
-                /*Pattern p = Pattern.compile(str1, Pattern.CASE_INSENSITIVE);
-                Matcher m = p.matcher(s);
-                while(m.find()){
-                    list.add(m.group());
-                }*/
-            }
-            //System.out.println(list);
-
         }catch (Exception e){
             e.printStackTrace();
         }
-
-        return result;
+        for(int i = 0;i < list.size(); i++){
+            System.out.println(list.get(i).printResult());
+        }
+        return list;
     }
     //读取目录下的日志
     private static String[] readFile(String path){
