66850d94a3cc7f198ffffa67da83ef20c29b0e71
ZYF <m13110683916@163.com>
2020-02-09 17:03:22 +0800

    test: 添加CommandLine测试

--- 221701233/test/SuiteTest.java
+++ 221701233/test/SuiteTest.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
         CommandFactoryTest.class,
-        ListCheckerTest.class
+        ListCheckerTest.class,
+        CommandLineTest.class,
 })
 public class SuiteTest {
 }
