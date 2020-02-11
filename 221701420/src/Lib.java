a1faa5c00092624ddf400060039cd66011689d66
Seway <47681078+Seway@users.noreply.github.com>
2020-02-11 11:27:55 +0800

    完成了日期排序函数
    完成了对指定目录下读取的文件数组的排序

--- 221701420/src/Lib.java
+++ 221701420/src/Lib.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 //储存省份患者信息
 class province_info{
@@ -14,12 +14,31 @@ public class Lib {
     public Lib() throws IOException {
         //初始化数据数组
         ArrayList<province_info> infos = new ArrayList<province_info>();
-
+        sort_file();
     }
     //对指定目录下的文件按照日期进行排序
-    private void sort_file(){
-        File file=new File("C:\\Users\\58215\\Desktop\\新建文本文档.txt");//创建指定目录的File对象
+    private void sort_file() throws IOException {
+        File file=new File("C:\\Users\\58215\\Desktop\\log");//创建指定目录的File对象
+        File[] files = file.listFiles();
+        List fileList = Arrays.asList(files);
 
+        //按照日期升序对file对象进行排序
+        Collections.sort(fileList, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                if (o1.isDirectory() && o2.isFile())
+                    return -1;
+                if (o1.isFile() && o2.isDirectory())
+                    return 1;
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        //读取排序好的文件对象并进行处理
+        for (File file_temp : files) {
+            System.out.println(file_temp.getName());
+            read_file(file_temp);//读取指定的文件
+        }
     }
 
     //读取指定的文件(传入File对象)
