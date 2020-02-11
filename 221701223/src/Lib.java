e4f3db45b3db963e775a003170745f884377224a
YBN <493670735@qq.com>
2020-02-11 16:17:35 +0800

    第二次commit，完善了命令行参数解析

--- 221701223/src/Lib.java
+++ 221701223/src/Lib.java
@@ -1,8 +1,10 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 
 /**
@@ -38,6 +40,15 @@ public class Lib {
             return false;
         }
         return true;
+    }
+
+    public static int getIndexFromStrings(String[] strings, String target) {
+        for (int i = 0; i < strings.length; i++) {
+            if (target.equals(strings[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
 
@@ -137,11 +148,17 @@ class RecordContainer {
  */
 class ArgumentParser {
 
-    private static final String[] PROVINCE_LIST = {"安徽", "澳门", "北京", "重庆", "福建", "甘肃", "广东", "广西", "贵州",
+    public static final HashSet<String> COMMAND_LIST = new HashSet<String>() {{
+        add("-date");
+        add("-type");
+        add("-province");
+        add("-log");
+        add("-out");
+    }};
+    private static final String[] PROVINCE_LIST = {"全国", "安徽", "澳门", "北京", "重庆", "福建", "甘肃", "广东", "广西", "贵州",
         "哈尔滨", "海南", "河北", "河南", "湖北", "湖南", "吉林", "江苏", "江西", "内蒙古", "宁夏", "青海", "山东", "山西",
         "山西", "上海", "四川", "台湾", "天津", "西藏", "香港", "新疆", "云南", "浙江"
     };
-    private static final String[] COMMAND_LIST = {"-date", "-type", "-province", "-log", "-out"};
     /**
      * Original arguments
      */
@@ -158,21 +175,16 @@ class ArgumentParser {
 
     public Command makeCommand() {
         String date = getDate();
-        String logPath = getArgumentByCommand("-log");
-        String outPah = getArgumentByCommand("-out");
+        String logPath = originalArguments[getIndexOfCommand("-log") + 1];
+        String outPah = originalArguments[getIndexOfCommand("-out") + 1];
         ArrayList<String> patientType = getPatientType();
+        ArrayList<String> provinceList = getProvinces();
 
-        return new Command(logPath, outPah, date, patientType);
+        return new Command(logPath, outPah, date, patientType, provinceList);
     }
 
     private int getIndexOfCommand(String command) {
-        return Arrays.binarySearch(originalArguments, command);
-    }
-
-    private String getArgumentByCommand(String arg) {
-
-        int index = getIndexOfCommand(arg);
-        return originalArguments[index + 1];
+        return Lib.getIndexFromStrings(originalArguments, command);
     }
 
     private String getDate() {
@@ -188,7 +200,7 @@ class ArgumentParser {
         if (Lib.validateFormatOfDateString(originalArguments[index + 1])) {
             return originalArguments[index + 1];
         } else {
-            System.err.println("-list参数错误，无效的日期格式。");
+            System.err.println("-date:参数错误，无效的日期格式。");
             return "null";
         }
     }
@@ -196,11 +208,18 @@ class ArgumentParser {
     private ArrayList<String> getPatientType() {
 
         int index = getIndexOfCommand("-type");
+        //index<0表明命令行参数中不含-type命令，就在type数组写一个"null"然后返回
         if (index < 0) {
             return new ArrayList<String>(1) {{
-                add("null");
+                add("所有");
             }};
         }
+        //如果args中-type的下一个元素也是一条命令选项，则表明-type命令没有参数，报错
+        if (COMMAND_LIST.contains(originalArguments[index + 1])) {
+            System.err.println("-type:参数不能为空！");
+            System.exit(-1);
+        }
+
         HashMap<String, String> patientTypeMap = new HashMap<String, String>(4) {{
             put("ip", "感染患者");
             put("sp", "疑似患者");
@@ -214,18 +233,43 @@ class ArgumentParser {
             if (patientTypeMap.get(originalArguments[index + 1]) != null) {
                 patientTypeList.add(patientTypeMap.get(originalArguments[index + 1]));
                 index++;
-            } else if (Arrays.binarySearch(COMMAND_LIST, originalArguments[index]) < 0) {
-                System.err.println("-type:参数" + originalArguments[index + 1] + "无效！");
-                System.exit(-1);
-                break;
             } else {
-                System.err.println("-type:参数不能为空！");
-                System.exit(-1);
                 break;
             }
         }
 
         return patientTypeList;
+    }
+
+    @NotNull
+    private ArrayList<String> getProvinces() {
+
+        int index = getIndexOfCommand("-province");
+        //index<0表明命令行参数中不含-province命令，在province数组写默认的选项"全国"
+        if (index < 0) {
+            return new ArrayList<String>(1) {{
+                add("全国");
+            }};
+        }
+        //如果args中-type的下一个元素也是一条命令选项，则表明-type命令没有参数，报错
+        if (COMMAND_LIST.contains(originalArguments[index + 1])) {
+            System.err.println("-province:参数不能为空！");
+            System.exit(-1);
+        }
+
+        ArrayList<String> provinceList = new ArrayList<String>();
+
+        while (true) {
+            //传入的参数和可用参数列表进行比对，get方法不返回null则取出参数对应的中文字符串加入List
+            if (Lib.getIndexFromStrings(PROVINCE_LIST, originalArguments[index + 1]) >= 0) {
+                provinceList.add(originalArguments[index + 1]);
+                index++;
+            } else {
+                break;
+            }
+        }
+
+        return provinceList;
     }
 }
 
@@ -243,11 +287,12 @@ class Command {
     ArrayList<String> type;
     ArrayList<String> province;
 
-    Command(String logPath, String outPath, String date, ArrayList<String> patientType) {
+    Command(String logPath, String outPath, String date, ArrayList<String> patientType, ArrayList<String> provinceList) {
         this.date = date;
         this.log = logPath;
         this.out = outPath;
         this.type = patientType;
+        this.province = provinceList;
     }
 
     public void show() {
@@ -255,6 +300,9 @@ class Command {
         System.out.println(log);
         System.out.println(out);
         for (String s : type) {
+            System.out.println(s);
+        }
+        for (String s : province) {
             System.out.println(s);
         }
     }
