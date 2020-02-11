00d4b9401ba35c952931a60cf3099820a5234c98
Just-hy <525427312@qq.com>
2020-02-11 22:34:21 +0800

    half down

--- 221701122/src/InfectStatistic.java
+++ 221701122/src/InfectStatistic.java
@@ -1,3 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.io.File;
+
 /**
  * InfectStatistic TODO
  *
@@ -7,6 +16,7 @@
  */
 class InfectStatistic {
 
+	int[][] c = new int[23][4];// 整体数据结构
 	private String c1 = null;
 	private String c2 = null;
 	private String c3 = null;
@@ -22,7 +32,13 @@ class InfectStatistic {
 		 */
 		if (infectstatistic.isList(args[0])) {
 			infectstatistic.beginAnalysis(args);
-			infectstatistic.runNing();
+			try {
+				infectstatistic.readFile();// 打开一个文件并处理相关数据
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("can't find log file");
+			}
 			return;
 		} else {
 			return;
@@ -31,7 +47,6 @@ class InfectStatistic {
 
 	private void beginAnalysis(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("www");
 		int c = 0;
 		for (int i = 1; i < args.length; i++) {
 			if (isCommand(args[i])) {
@@ -77,6 +92,117 @@ class InfectStatistic {
 		}
 	}
 
+	void readFile() throws FileNotFoundException {
+		File file = new File("C:/Users/hc201804/Desktop/git工作目录/InfectStatistic-main/221701122/log/2020-01-22.log.txt");
+		if (!file.exists()) {
+			System.out.println("节点基本信息文件未找到");
+			System.exit(0);
+		}
+		Scanner s = new Scanner(file);
+		while (s.hasNext()) {
+			String province = s.next();
+			// int num=s.nextInt();
+			// String de = s.next();
+			Province pr = null;
+			try {
+				pr = Province.valueOf(province);
+			} catch (Exception ex) {
+				System.out.println("文件格式错误（省份读入异常）");
+				System.exit(0);
+			}
+			System.out.println(pr);// 获取当前的省份
+			/*
+			 * c[pr.ordinal()][1]++; System.out.println(c[1][1]);
+			 */
+			String second = s.next();
+			if (second.equals("新增")) {
+				String third = s.next();
+				if (third.equals("感染患者")) {
+					String data = s.next();
+					int i = stringToInt(data);
+					c[pr.ordinal()][0] += i;// 省份新增感染患者
+					c[0][0] += i;// 全国新增感染患者
+				} else if (third.equals("疑似患者")) {
+					String data = s.next();// 行末
+					int i = stringToInt(data);
+					c[pr.ordinal()][1] += i;// 省份新增疑似患者
+					c[0][1] += i;// 全国新增疑似患者
+				} else {
+					System.out.println("文件格式错误（新增患者仅有感染患者与疑似患者两种）");
+					System.exit(0);
+				}
+			} else if (second.equals("感染患者")) {
+				String third = s.next();
+				if (third.equals("流入")) {
+					String next_province = s.next();
+					Province next_pr = null;
+					try {
+						next_pr = Province.valueOf(next_province);
+					} catch (Exception ex) {
+						System.out.println("文件格式错误（感染患者流入省份读入异常）");
+						System.exit(0);
+					}
+					String data = s.next();// 行末
+					int i = stringToInt(data);
+					c[pr.ordinal()][0] -= i; // 流出的省份減少
+					c[next_pr.ordinal()][0] += i;// 流入的省份增加
+				} else {
+					System.out.println("文件格式错误（省份 感染患者 后必为\"流入\"）");
+					System.exit(0);
+				}
+			} else if (second.equals("疑似患者")) {
+				String third = s.next();
+				if (third.equals("流入")) {
+					String next_province = s.next();
+					Province next_pr = null;
+					try {
+						next_pr = Province.valueOf(next_province);
+					} catch (Exception ex) {
+						System.out.println("文件格式错误（疑似患者流入省份读入异常）");
+						System.exit(0);
+					}
+					String data = s.next();// 行末
+					int i = stringToInt(data);
+					c[pr.ordinal()][1] -= i; // 流出的省份減少
+					c[next_pr.ordinal()][1] += i;// 流入的省份增加
+				} else if (third.equals("确诊感染")) {
+					String data = s.next();// 行末
+					int i = stringToInt(data);
+					c[pr.ordinal()][1] -= i;// 疑似患者减少
+					c[pr.ordinal()][0] += i;// 感染患者增加
+				} else {
+					System.out.println("文件格式错误（省份 疑似患者 后必为\"流入\"或\"确诊感染\"）");
+					System.exit(0);
+				}
+			} else if (second.equals("死亡")) {
+				String data = s.next();// 行末
+				int i = stringToInt(data);
+				c[pr.ordinal()][0] -= i;// 感染患者减少
+				c[pr.ordinal()][3] += i;// 死亡人数增加
+			} else if (second.equals("治愈")) {
+				String data = s.next();// 行末
+				int i = stringToInt(data);
+				c[pr.ordinal()][0] -= i;// 感染患者减少
+				c[pr.ordinal()][2] += i;// 治愈人数增加
+			} else if (second.equals("排除")) {
+				String third = s.next();
+				if (third.equals("疑似患者")) {
+					String data = s.next();// 行末
+					int i = stringToInt(data);
+					c[pr.ordinal()][1] -= i;// 疑似患者减少
+				} else {
+					System.out.println("文件格式错误（省份 排除 后必为\"疑似患者\"）");
+					System.exit(0);
+				}
+			} else {
+				System.out.println("文件格式错误（省份后第一个参数错误）");
+				System.exit(0);
+			}
+		}
+		s.close();
+	}
+
+	
 	boolean isCommand(String args) {
 		if (args.charAt(0) == '-') {
 			return true;
@@ -126,8 +252,21 @@ class InfectStatistic {
 		return false;
 	}
 
-	void runNing() {
-
+	// 将例如String 2人转为int 2
+	int stringToInt(String data) {
+		int i = 0;
+		try {
+			data = data.substring(0, data.length() - 1);
+			i = Integer.parseInt(data);
+		} catch (NumberFormatException ex) {
+			System.out.println("人数格式错误");
+			System.exit(0);
+		}
+		return i;
 	}
 
+}
+
+enum Province {
+	全国, 福建, 河北, 山西, 辽宁, 吉林, 黑龙江, 浙江, 江苏, 安徽, 江西, 山东, 河南, 湖北, 湖南, 广东, 海南, 四川, 贵州, 云南, 陕西, 甘肃, 青海, 台湾;
 }
