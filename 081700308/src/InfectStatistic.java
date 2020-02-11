8e222aea29d209354e11994392f9152f1b4fdc3d
fuqijia <60819777+fuqijia@users.noreply.github.com>
2020-02-11 22:24:22 +0800

    2

--- 081700308/src/InfectStatistic.java
+++ 081700308/src/InfectStatistic.java
@@ -7,7 +7,52 @@
  * @since xxx
  */
 class InfectStatistic {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
+		
+		static String[] provinces= {
+				"全国",
+				"安徽",
+				"北京",
+				"重庆",
+				"福建",
+				"甘肃",
+				"广东",
+				"广西",
+				"贵州",
+				"海南",
+				"河北",
+				"河南",
+				"黑龙江",
+				"湖北",
+				"湖南",
+				"吉林",
+				"江苏",
+				"江西",
+				"辽宁",
+				"内蒙古",
+				"宁夏",
+				"青海",
+				"山东",
+				"山西",
+				"陕西",
+				"上海",
+				"四川",
+				"天津",
+				"西藏",
+				"新疆",
+				"云南",
+				"浙江"};
+		
+		int[] ip=new int[32];
+		int[] sp=new int[32];
+		int[] cure=new int[32];
+		int[] dead=new int[32];
+		
+		 public static void main(String args[ ])
+		    {
+		       System.out.println(provinces[31]);
+		    } // 方法main结束
+		
+		
+	
+
 }
