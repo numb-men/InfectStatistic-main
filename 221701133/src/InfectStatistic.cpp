f26500b19cf7070f5a9e0fb388a8a184ebecdbc9
17lhf <60611483+17lhf@users.noreply.github.com>
2020-02-12 18:51:48 +0800

    更新InfectStatistic.cpp
    新增地区的各个情况的人数变化方法，新增地区信息的输出和地区列表信息的输出

--- 221701133/src/InfectStatistic.cpp
+++ 221701133/src/InfectStatistic.cpp
@@ -7,18 +7,10 @@
 
 using namespace std;
 
-//用于记录时关于地区的顺序模板
 const string REGIONNAME[] = { "全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南",
 "河北","河南","黑龙江","湖北","湖南","吉林","江苏","江西","辽宁","内蒙古","宁夏","青海","山东","山西",
-"陕西","上海","四川","天津","西藏","新疆","云南","浙江" };
+"陕西","上海","四川","天津","西藏","新疆","云南","浙江" };//用于记录时关于地区的顺序模板
 const int REGIONNUM = sizeof(REGIONNAME) / sizeof(REGIONNAME[0]);//地区数
-
-int main(int argc, char* argv[])
-{
-
-    std::cout << "Hello World!\n"; 
-	return 0;
-}
 
 class Regions //地区类
 {
@@ -31,6 +23,11 @@ public:
 	Regions(void);
 	Regions(string n);
 	Regions(string n, int c, int s, int cue, int d);
+	bool confirmedPeopleAdd(const int &n);//增加确诊人数
+	bool suspectPeopleAdd(const int &n);//增加疑似人数
+	bool curePeopleAdd(const int &n);//增加治愈人数
+	bool deathPeopleAdd(const int &n);//增加死亡人数
+	friend ostream &operator<<(ostream &out,const Regions &r);//用以输出该地区的疫情信息
 };
 
 class StatisticList //疫情统计表
@@ -38,8 +35,9 @@ class StatisticList //疫情统计表
 	Regions regionsList[REGIONNUM];//地区情况统计表
 public:
 	StatisticList();
+	friend bool readLogMessage(StatisticList &slist);//读取日志文件并存储
+	friend ostream &operator<<(ostream &out, const StatisticList &s);//用以输出地区列表的疫情信息
 };
-
 
 Regions::Regions(string n, int c, int s, int cue, int d)
 {
@@ -68,9 +66,86 @@ Regions::Regions(void)
 	death = 0;
 }
 
+bool Regions::confirmedPeopleAdd(const int &n)//确诊人数增加
+{
+	if (confirmed += n)
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+bool Regions::suspectPeopleAdd(const int &n)//增加疑似人数
+{
+	if (suspect += n) 
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+bool Regions::curePeopleAdd(const int &n)//增加治愈人数
+{
+	if (cure += n) 
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+bool Regions::deathPeopleAdd(const int &n)//增加死亡人数
+{
+	if (death += n) 
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
 StatisticList::StatisticList()
 {
-	for (int i = 0; i < REGIONNUM; i++) {
+	for (int i = 0; i < REGIONNUM; i++) //默认初始化为各个省份的所有人数参数为零
+	{
 		regionsList[i] = Regions(REGIONNAME[i]);
 	}
+}
+
+ostream &operator<<(ostream &out, const Regions &r)//用以输出该地区的疫情信息
+{
+	out << r.name<<" 感染患者"<<r.confirmed<<"人 疑似患者"<<r.suspect<<"人 治"
+		"愈"<<r.cure<<"人 死亡"<<r.death<<"人\n";
+	return out;
+}
+
+ostream &operator<<(ostream &out, const StatisticList &s)//用以输出地区列表的疫情信息
+{
+	for (int i = 0; i < REGIONNUM; i++) //默认初始化为各个省份的所有人数参数为零
+	{
+		out << s.regionsList[i];
+	}
+	return out;
+}
+
+bool readLogMessage(StatisticList &slist)//读取日志文件并存储
+{
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "Hello World!\n";
+	return 0;
 }
