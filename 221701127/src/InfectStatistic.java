28538dec4d229ebf14f1af49346c77be5c734eb7
XTG-L <XTG-L@github.com>
2020-02-12 19:47:36 +0800

    在实现命令时遇到问题，头裂开，先作为一个版本
    从文件中读取的数据跟代码内编的字符串匹配不上，编码应该都是utf-8，正在查错。

--- 221701127/src/InfectStatistic.java
+++ 221701127/src/InfectStatistic.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 /**
  * InfectStatistic
- *
  * @author XTG-L
  * @version 0.2
  * @since 2020-2-8
@@ -17,16 +16,23 @@ class InfectStatistic {
     public static void main(String[] args) {
         CmdList cmd = new CmdList(args);
         Log log = new Log(cmd);
-        ArrayList<String> as = log.getLogs();
-        for (String string : as) {
-            System.out.println(string);
+        log.logToRegion(log.getLogs());
+        // ArrayList<String> logs = log.getLogs();
+        // System.out.println(logs.get(0).equals("福建 新增 感染患者 2人"));
+        for (Log.Region region : log.getRegions()) {
+            System.out.println(
+                region.getName()+" "+
+                region.getIp()+" "+
+                region.getSp()+" "+
+                region.getCure()+" "+
+                region.getDead()
+            );
         }
     }
 }
 
 /**
  * CmdArgs
- * 
  * 处理命令
  */
 class CmdArgs {
@@ -34,7 +40,6 @@ class CmdArgs {
 
     /**
      * 传入命令行参数数组构造
-     * 
      * @param args
      */
     CmdArgs(String[] args) {
@@ -51,7 +56,6 @@ class CmdArgs {
 
     /**
      * 获取命令
-     * 
      * @return
      */
     String getCmd() {
@@ -64,7 +68,6 @@ class CmdArgs {
 
     /**
      * 获取某个命令行参数的值，多个值只会返回第一个
-     * 
      * @param key
      * @return
      */
@@ -74,7 +77,6 @@ class CmdArgs {
 
     /**
      * 获取某个命令行参数的值，返回列表
-     * 
      * @param key
      * @return
      */
@@ -99,7 +101,6 @@ class CmdArgs {
 
     /**
      * 判断该命令是否有对应的参数
-     * 
      * @param key
      * @return
      */
@@ -120,7 +121,6 @@ class CmdArgs {
 
     /**
      * 判断输入的命令是否符合规范
-     * 
      * @return
      */
     boolean isLegalCmd() {
@@ -152,12 +152,11 @@ class CmdArgs {
 
 /**
  * CmdList
- * 
  * 处理list命令
  */
 class CmdList extends CmdArgs {
-    private String logPath = null;
-    private String outPath = null;
+    private String logPath = null;  //日志文件路径
+    private String outPath = null;  //输出文件路径
 
     /**
      * 传入list命令
@@ -174,7 +173,6 @@ class CmdList extends CmdArgs {
 
     /**
      * 获取日志文件的路径
-     * 
      * @return
      */
     String getLogPath() {
@@ -183,7 +181,6 @@ class CmdList extends CmdArgs {
 
     /**
      * 获取输出文件的路径
-     * 
      * @return
      */
     String getOutPath() {
@@ -192,7 +189,6 @@ class CmdList extends CmdArgs {
 
     /**
      * 判断list命令是否符合规范
-     * 
      * @return
      */
     boolean isLegal() {
@@ -221,19 +217,18 @@ class CmdList extends CmdArgs {
 
 /**
  * Log
- * 
  * 处理日志文件
  */
 class Log {
-    private String logPath = null;
-    private File logDir = null;
-    private String[] filesName = null;
-    private ArrayList<String> logs = new ArrayList<>();
-    private String ignore = "//.*";
+    private String logPath = null;  //日志文件路径
+    private File logDir = null; //日志文件的文件夹
+    private String[] filesName = null;  //日志文件的文件名
+    private ArrayList<String> logs = new ArrayList<>(); //用于储存日志文件的动态数组
+    private String ignore = "//.*"; //行忽略标志
+    private ArrayList<Region> regions = new ArrayList<>();  //用于储存地区类的动态数组
 
     /**
      * 传入命令类并初始化日志文件的路径和文件列表
-     * 
      * @param cmd
      */
     Log(CmdList cmdList) {
@@ -251,13 +246,29 @@ class Log {
 
     /**
      * 获取日志文件的文件名
-     * 
      * @return
      */
     String[] getFilesName() {
         return this.filesName;
     }
 
+    /**
+     * 获取日志文件数据
+     * @return
+     */
+    ArrayList<String> getLogs() {
+        return this.logs;
+    }
+
+    ArrayList<Region> getRegions() {
+        return this.regions;
+    }
+
+    /**
+     * 读取日志文件并储存在logs里，然后调用logToRegion将日志文件转换并保存
+     * @param logPath
+     * @throws IOException
+     */
     void readLog(String logPath) throws IOException {
         BufferedReader br = null;
         String line = null;
@@ -266,29 +277,117 @@ class Log {
             InputStreamReader isr = new InputStreamReader(fis, "utf-8");
             br = new BufferedReader(isr);
             while((line = br.readLine()) != null) {
-                if(delIgnore(line) != null) {
+                if(!isIgnore(line)) {
                     logs.add(line);
                 }
             }
             br.close();
         }
+        logToRegion(logs);
     }
 
-    ArrayList<String> getLogs() {
-        return this.logs;
-    }
-
-    String delIgnore(String line) {
-        String result = line;
+    /**
+     * 判断一行日志是否为注释行
+     * @param line
+     * @return
+     */
+    boolean isIgnore(String line) {
+        boolean result = false;
         if(Pattern.matches(ignore, line)) {
-            result = null;
+            result = true;
         }
         return result;
     }
 
-    class Region {
-        private String name;
-        private int ip, sp, cure, dead; //感染患者， 疑似患者， 治愈， 死亡患者
+    /**
+     * 将日志文件转换为地区类并储存
+     * @param logs
+     */
+    void logToRegion(ArrayList<String> logs) {
+        for (String log : logs) {
+            String[] items = log.split(" ");
+            Region tmpRegion = new Region(items[0]);
+            if(!regions.contains(tmpRegion)) {
+                Region region = new Region(items[0]);
+                regions.add(region);
+            }
+            Region region = regions.get(regions.indexOf(tmpRegion));
+            // for (String item : items) {
+            //     System.out.print(item);
+            // }
+            // System.out.print(items[1]);
+            // System.out.println("新增".equals(items[1]));
+            // items[1] = "新增";
+            switch (items[1]) {
+                case "新增": {
+                    items[3].replace("人", "");
+                    System.out.println(items[3]);
+                    if(items[2].equals("感染患者")) {
+                        region.setIp(region.getIp() + Integer.valueOf(items[3]));
+                    } else if(items[2].equals("疑似患者")) {
+                        region.setSp(region.getSp() + Integer.valueOf(items[3]));
+                    }
+                    break;
+                }
+                case "疑似患者": {
+                    items[4].replace("人","");
+                    region.setSp(region.getSp() - Integer.valueOf(items[4]));
+                    break;
+                }
+                case "感染患者": {
+                    items[4].replace("人","");
+                    break;
+                }
+                case "治愈": {
+                    break;
+                }
+                case "死亡": {
+                    break;
+                }
+                case "排除": {
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Region
+     * 用于暂存日志文件内容的类对象
+     */
+    public class Region {
+        private String name, another;   //地区名称，可能会出现的另一个地区的名称
+        private int ip, sp, cure, dead; //感染患者，疑似患者，治愈，死亡患者
+
+        /**
+         * 传入地区名称
+         * @param name
+         */
+        Region(String name) {
+            this.name = name;
+            this.another = null;
+            this.ip = 0;
+            this.sp = 0;
+            this.cure = 0;
+            this.dead = 0;
+        }
+
+        /**
+         * 重载equals函数，用于排序和判断
+         * @param obj
+         * @return
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Region) {
+                Region region = (Region) obj;
+                return this.name.equals(region.name);
+            }
+            return super.equals(obj);
+        }
 
         /**
          * 获取地区名称
@@ -296,6 +395,22 @@ class Log {
          */
         String getName() {
             return this.name;
+        }
+
+        /**
+         * 获取另一个地区的名称
+         * @return
+         */
+        String getAnother() {
+            return this.another;
+        }
+
+        /**
+         * 设置另一个地区的名称
+         * @param another
+         */
+        void setAnother(String another) {
+            this.another = another;
         }
 
         /**
@@ -307,11 +422,27 @@ class Log {
         }
 
         /**
+         * 设置地区感染患者人数
+         * @param ip
+         */
+        void setIp(int ip) {
+            this.ip = ip;
+        }
+
+        /**
          * 获取地区疑似患者人数
          * @return
          */
         int getSp() {
             return this.sp;
+        }
+
+        /**
+         * 设置地区疑似患者人数
+         * @param sp
+         */
+        void setSp(int sp) {
+            this.sp = sp;
         }
 
         /**
@@ -323,11 +454,27 @@ class Log {
         }
 
         /**
+         * 设置地区治愈人数
+         * @param cure
+         */
+        void setCure(int cure) {
+            this.cure = cure;
+        }
+
+        /**
          * 获取地区死亡患者人数
          * @return
          */
         int getDead() {
             return this.dead;
+        }
+
+        /**
+         * 设置地区死亡人数
+         * @param dead
+         */
+        void setDead(int dead) {
+            this.dead = dead;
         }
     }
 }
