06bfc556d864ced4c28d82cbb0a95fc8a50f0fbe
cunkoulaocai <30389241+cunkoulaocai@users.noreply.github.com>
2020-02-14 19:16:27 +0800

    删除错误提交的文件command

---
+++ 221701139/src/Lib.java
@@ -0,0 +1,13 @@
+/**
+ * Lib
+ * TODO
+ *
+ * @author xxx
+ * @version xxx
+ * @since xxx
+ */
+public class Lib {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
