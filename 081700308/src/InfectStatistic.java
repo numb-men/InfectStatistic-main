4432dfec07ea48351f89a2e4c678c70913d3d485
fuqijia <60819777+fuqijia@users.noreply.github.com>
2020-02-14 11:22:10 +0800

    Update InfectStatistic.java

--- 081700308/src/InfectStatistic.java
+++ 081700308/src/InfectStatistic.java
@@ -1,4 +1,6 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 /**
  * InfectStatistic
@@ -44,44 +46,303 @@ class InfectStatistic {
 				"云南",
 				"浙江"};
 		
-		int[] ip=new int[32];
+        int[] ip=new int[32];
 		int[] sp=new int[32];
 		int[] cure=new int[32];
 		int[] dead=new int[32];
+		int[] exist=new int[32];
+		
+		public  int getIp(int n)
+		{
+			return ip[n];
+		}
+		
+		
 		
 		//省份对应下标
 		public int proToInt(String p)
 		{
-			
+			switch(p)
+			{
+			    case "全国" :
+			    	return 0;
+			    case "安徽" :
+			    	return 1;
+			    case "北京" :
+			    	return 2;
+			    case "重庆" :
+			    	return 3;
+			    case "福建" :
+			    	return 4;
+			    case "甘肃" :
+			    	return 5;
+			    case "广东" :
+			    	return 6;
+			    case "广西" :
+			    	return 7;
+			    case "贵州" :
+			    	return 8;
+			    case "海南" :
+			    	return 9;
+			    case "河北" :
+			    	return 10;
+			    case "河南" :
+			    	return 11;
+			    case "黑龙江" :
+			    	return 12;
+			    case "湖北" :
+			    	return 13;
+			    case "湖南" :
+			    	return 14;
+			    case "吉林" :
+			    	return 15;
+			    case "江苏" :
+			    	return 16;
+			    case "江西" :
+			    	return 17;
+			    case "辽宁" :
+			    	return 18;
+			    case "内蒙古" :
+			    	return 19;
+			    case "宁夏" :
+			    	return 20;
+			    case "青海" :
+			    	return 21;
+			    case "山东" :
+			    	return 22;
+			    case "山西" :
+			    	return 23;
+			    case "陕西" :
+			    	return 24;
+			    case "上海" :
+			    	return 25;
+			    case "四川" :
+			    	return 26;
+			    case "天津" :
+			    	return 27;
+			    case "西藏" :
+			    	return 28;
+			    case "新疆" :
+			    	return 29;
+			    case "云南" :
+			    	return 30;
+			    case "浙江" :
+			    	return 31;
+			}
 			return 0;
 		}
 		
+		//下标对应省份
+		public String intToPro(int n)
+		{
+			switch(n)
+			{
+			    case 0 :
+			    	return "全国";
+			    case 1 :
+			    	return "安徽";
+			    case 2 :
+			    	return "北京";
+			    case 3 :
+			    	return "重庆";
+			    case 4 :
+			    	return "福建";
+			    case 5 :
+			    	return "甘肃";
+			    case 6 :
+			    	return "广东";
+			    case 7 :
+			    	return "广西";
+			    case 8 :
+			    	return "贵州";
+			    case 9 :
+			    	return "海南";
+			    case 10 :
+			    	return "河北";
+			    case 11 :
+			    	return "河南";
+			    case 12 :
+			    	return "黑龙江";
+			    case 13 :
+			    	return "湖北";
+			    case 14 :
+			    	return "湖南";
+			    case 15 :
+			    	return "吉林";
+			    case 16 :
+			    	return "江苏";
+			    case 17 :
+			    	return "江西";
+			    case 18 :
+			    	return "辽宁";
+			    case 19 :
+			    	return "内蒙古";
+			    case 20 :
+			    	return "宁夏";
+			    case 21 :
+			    	return "青海";
+			    case 22 :
+			    	return "山东";
+			    case 23 :
+			    	return "山西";
+			    case 24 :
+			    	return "陕西";
+			    case 25 :
+			    	return "上海";
+			    case 26 :
+			    	return "四川";
+			    case 27 :
+			    	return "天津";
+			    case 28 :
+			    	return "西藏";
+			    case 29 :
+			    	return "新疆";
+			    case 30 :
+			    	return "云南";
+			    case 31 :
+			    	return "浙江";
+			}
+			return "全国";
+		}
 	
     	
-	 static String type1="\\w+ 新增 感染患者 \\d+人";
-		String type2="\\w+ 新增 疑似患者 \\d+人";
-		String type3="\\w+ 感染患者 流入 \\w+ \\d+人";
-		String type4="\\w+ 疑似患者 流入 \\w+ \\d+人";
-		String type5="\\w+ 死亡 \\d+人";
-		String type6="\\w+ 治愈 \\d+人";
-		String type7="\\w+ 疑似患者 确诊感染 \\d+人";
-		String type8="\\w+ 排除 疑似患者 \\d+人";
-		
-		public static String[] getAddIp(String text)
+	static String type1=".*新增 感染患者.*";
+	static	String type2=".*新增 疑似患者.*";
+	static	String type3=".*感染患者 流入.*";
+	static	String type4=".*疑似患者 流入.*";
+	static	String type5=".*死亡.*";
+	static	String type6=".*治愈.*";
+	static	String type7=".*疑似患者 确诊感染.*";
+	static	String type8=".*排除 疑似患者.*";
+		static Pattern pattern8=Pattern.compile(type8);
+		Pattern pattern1=Pattern.compile("人");
+		Pattern pattern2=Pattern.compile(" ");
+		//统计新增感染患者
+		public  void addIp(String text)
 		{
-			Pattern pattern1=Pattern.compile("(.*) 新增");
-			Pattern pattern2=Pattern.compile("新");
-			return pattern2.split(text);//getString(pattern1,pattern2);
+			
+	
+			//提取句首省份
+			String pro=pattern2.split(text)[0];
+			//提取句末人数
+			int addip=Integer.parseInt(pattern1.split(pattern2.split(text)[3])[0]);
+			//对ip数组进行操作
+			ip[proToInt(pro)]+=addip;
+			exist[proToInt(pro)]=1;
 		}
     
-		 public static void main(String args[ ])
-		    {
+		public  void addSp(String text)
+		{
+			
+			
+			//提取句首省份
+			String pro=pattern2.split(text)[0];
+			//提取句末人数
+			int addsp=Integer.parseInt(pattern1.split(pattern2.split(text)[3])[0]);
+			//对sp数组进行操作
+			sp[proToInt(pro)]+=addsp;
+			exist[proToInt(pro)]=1;
+		}
+		
+		public  void transIp(String text)
+		{
+			
+			
+			//提取源省份
+			String pro1= pattern2.split(text)[0];
+			//提取转移省份
+			String pro2= pattern2.split(text)[3];
+			//提取句末人数
+			int transip=Integer.parseInt(pattern1.split(pattern2.split(text)[4])[0]);
+			//对ip数组操作
+			ip[proToInt(pro1)]-=transip;
+			ip[proToInt(pro2)]+=transip;
+			exist[proToInt(pro1)]=1;
+			exist[proToInt(pro2)]=1;
+		}
+		
+		public  void transSp(String text)
+		{
+			//提取源省份
+			String pro1= pattern2.split(text)[0];
+			//提取转移省份
+			String pro2= pattern2.split(text)[3];
+			//提取句末人数
+			int transsp=Integer.parseInt(pattern1.split(pattern2.split(text)[4])[0]);
+			//对sp数组操作
+			sp[proToInt(pro1)]-=transsp;
+			sp[proToInt(pro2)]+=transsp;
+			exist[proToInt(pro1)]=1;
+			exist[proToInt(pro2)]=1;
+		}
+		
+		public  void addDead(String text)
+		{
+			//提取句首省份
+			String pro= pattern2.split(text)[0];
+			//提取句末人数
+			int newdead=Integer.parseInt(	pattern1.split(pattern2.split(text)[2])[0]);
+			//对ip dead数组操作
+			int d=proToInt(pro);
+			dead[d]+=newdead;
+			ip[d]-=newdead;
+			exist[d]=1;
+		}
+		
+		public  void addCure(String text)
+		{
+			//提取句首省份
+			String pro= pattern2.split(text)[0];
+			//提取句末人数
+			int newcure= Integer.parseInt(	pattern1.split(pattern2.split(text)[2])[0]);
+			//对ip cure数组操作
+			int d=proToInt(pro);
+			cure[d]+=newcure;
+			ip[d]-=newcure;
+			exist[d]=1;
+		}
+		
+		public void spToIp(String text)
+		{
+			//提取句首省份
+			String pro= pattern2.split(text)[0];
+			//提取句末人数
+			int newip= Integer.parseInt(	pattern1.split(pattern2.split(text)[3])[0]);
+			//对ip sp数组操作
+			int d=proToInt(pro);
+			ip[d]+=newip;
+			sp[d]-=newip;
+			exist[d]=1;
+		}
+		
+		public void  decSp(String text)
+		{
+			//提取句首省份
+			String pro= pattern2.split(text)[0];
+			//提取句末人数
+			int decsp= Integer.parseInt( 	pattern1.split(pattern2.split(text)[3])[0]);
+			//对sp数组操作
+			int d=proToInt(pro);
+			sp[d]-=decsp;
+			exist[d]=1;
+		}
+
+		
+		public static void main(String args[ ])
+		{
 			 
-			 String x="全国 新增 感染患者 1人";
-			 getAddIp(x);
-					
-		       System.out.println(getAddIp(x)[0]);
-		    }// 方法main结束
+			 String x="浙江 排除 疑似患者 7人";
+			// getAddIp(x);
+			 String y="123";
+			 Matcher m=pattern8.matcher(x);
+			 pattern8.pattern();
+			//boolean t = x.matches(type8);
+			int t=Integer.parseInt(y);
+			t++;
+			System.out.print(x.matches(type8));
+		       //System.out.print(getIp(0));
+		      // System.out.println(AddIp(x));
+		}// 方法main结束
 		
 		
 	
