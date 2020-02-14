298cbb4b5b417f0198f0fea3bfe3ef0fb3b11e38
lzy6666189 <60639011+lzy6666189@users.noreply.github.com>
2020-02-14 15:57:50 +0800

    第01次
    在根目录创建221701417文件夹并复制example文件目录

---
+++ 221701417/src/InfectStatistic.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author xxx
+ * @version xxx
+ * @since xxx
+ */
+class InfectStatistic {
+	
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+} 
