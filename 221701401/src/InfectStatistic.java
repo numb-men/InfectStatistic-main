f1bfd2239dd9f134ae345300a81bfacf81cbb210
itwillbefine <60869584+itwillbefine@users.noreply.github.com>
2020-02-14 20:28:44 +0800

    Fifth commit
    文件处理基本完成，准备写命令处理

--- 221701401/src/InfectStatistic.java
+++ 221701401/src/InfectStatistic.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
+import java.util.stream.Collectors;
 
 /**
  * InfectStatistic
@@ -13,13 +14,23 @@ import java.util.regex.*;
 class InfectStatistic {
     public static void main(String[] args) throws IOException {
     	FileHandle l=new FileHandle();
-    	l.findFile("D:\\testlog","2020-01-22");
+    	l.dealFile("D:/testlog","2020-01-27");
+    	l.writeFile("C:/Users/V556U/Desktop/a.txt");
     }
 }
 
-//文件处理类（八字没一撇）
+//命令行类
+class CmdHandle {
+	private String[] args;
+	
+	void setCmdArg(String[] arg) {
+		args=arg;
+	}
+}
+
+//文件处理类
 class FileHandle {
-	//private String pathname;//路径名
+	private List<LogResult> all=new ArrayList();
 	
 	//读文件
 	public void readFile(String path) throws IOException {
@@ -32,40 +43,54 @@ class FileHandle {
         	if(strTmp.startsWith("\uFEFF")){
         	   strTmp = strTmp.replace("\uFEFF","");
         	}//win10无法建立无BOM,用此去掉开头
-        	//这里进行结果统计
+        	//进行结果统计
         	dh.calResult(strTmp);
         }
-		List<LogResult> l=dh.calNationData();
-		for (LogResult a:l) {
-			System.out.printf("%s %d %d %d %d\n",a.getProvince(),a.getIp(),a.getSp(),a.getCure(),a.getDead());
-		}
-        buffReader.close();
-	}
+		List<LogResult> res=dh.calNationData();
+		all.addAll(res);
+		buffReader.close();
+	}		
 	
-	//找到日期对应文件(需修改为找到日期之前的文件)
-	public void findFile(String path,String date) throws IOException {
+	//找到日期对应文件进行处理
+	public void dealFile(String path,String date) throws IOException {
 		String[] list=new File(path).list();
+		boolean isEnd=false;
+		//按日期排序
+		List<String> fileList = Arrays.asList(list);
+	    Collections.sort(fileList, new Comparator<String>() {
+	    	@Override
+	    	public int compare(String o1, String o2) {
+	    		return o1.compareTo(o2);
+	    	}
+	    });
+	    //找文件
 		StringBuffer dt=new StringBuffer(date);
 		dt.append(".log.txt");
-		for (String file:list) {
-			if (file.contentEquals(dt)) {
-				StringBuffer ph=new StringBuffer(path);
-				ph.append("\\");
-				ph.append(dt);
-				readFile(ph.toString());				
-			}
+		for (String file:fileList) {
+			if (file.contentEquals(dt)) isEnd=true; 
+			StringBuffer ph=new StringBuffer(path);
+			ph.append("/");
+			ph.append(file);
+			readFile(ph.toString());
+			if (isEnd) break;
 		}
+		DataHandle dh=new DataHandle();
+		List<LogResult> res=dh.mergeResult(all);
+		all=res;
 	}
 	
-	//写文件(输入参数：路径和处理后的字符串)
-	public void writeFile(String path,String result) throws IOException {
+	//写文件(输入参数：路径)
+	public void writeFile(String path) throws IOException {
 		File file = new File(path);
 		if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
 		if(!file.exists()) file.createNewFile();
 		FileOutputStream fout=new FileOutputStream(file,true);
 		OutputStreamWriter writer=new OutputStreamWriter(fout,"UTF-8");
-		//这里要写入内容
-        //writer.write("zdfkjsdlf");
+		//这里要写入内容	
+		for (LogResult lr:all) {
+			String tmp=lr.toString();
+			writer.write(tmp);
+		}
         writer.flush();
         writer.close();
 	}
@@ -82,7 +107,7 @@ class LogResult {
 	private String[] order={"全国","安徽","北京","重庆","福建","甘肃","广东","广西",
 			   "贵州","海南","河北","河南","黑龙江","湖北","湖南","吉林","江苏","江西",
 			   "辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海","四川","天津",
-			   "西藏","新疆","云南","浙江"};
+			   "西藏","新疆","云南","浙江"};//指定的排序顺序
 	
 	LogResult() {
 		ip=sp=cure=dead=0;
@@ -129,6 +154,7 @@ class LogResult {
 		return dead;
 	}
 	
+	//增加ip,sp,cure,dead
 	void sum(int i,int s,int c,int d) {
 		ip+=i;
 		sp+=s;
@@ -136,6 +162,7 @@ class LogResult {
 		dead+=d;
 	}
 	
+	//得到省份在制定顺序中的次序
 	int getIndex (String str) {
 		for (int i=0;i<order.length;i++) {
 			if (str.equals(order[i])) return i;
@@ -143,8 +170,9 @@ class LogResult {
 		return -1;
 	}
 
+	//toString（字符串形式输出）
 	public String toString() {
-		return province+" "+ip+" "+sp+" "+cure+" "+dead;
+		return province+" 感染患者"+ip+"人 疑似患者"+sp+"人 治愈"+cure+"人 死亡"+dead+"人\n";
 	}
 }
 
@@ -157,11 +185,14 @@ class DataHandle {
 	protected Pattern addCure=Pattern.compile("([\\u4e00-\\u9fa5]{0,}+) 治愈 (\\d+)人");
 	protected Pattern spToIp=Pattern.compile("([\\u4e00-\\u9fa5]{0,}+) 疑似患者 确诊感染 (\\d+)人");
 	protected Pattern subSp=Pattern.compile("([\\u4e00-\\u9fa5]{0,}+) 排除 疑似患者 (\\d+)人");
-	//protected String typeSkip=".*/.*";//匹配//开头的行，方便过滤
 	private List<LogResult> list;
 	
 	DataHandle () {
 		list=new ArrayList<>();
+	}
+	
+	List<LogResult> getList() {
+		return list;
 	}
 	
 	//将单行文本转换成LogResult并加入集合
@@ -179,12 +210,12 @@ class DataHandle {
 			all.sum(lr.getIp(),lr.getSp(),lr.getCure(),lr.getDead());
 		}
 		list.add(all);
-		res=mergeResult();
+		res=mergeResult(list);
 		return res;
 	}
 	
 	//合并数据(参考)
-	public List<LogResult> mergeResult() {
+	public List<LogResult> mergeResult(List<LogResult> list) {
 		HashMap<String,LogResult> tempMap=new HashMap<String,LogResult>();
         for (LogResult lr:list) {
             String tmpStr=lr.getProvince();
@@ -203,7 +234,7 @@ class DataHandle {
             }
             
         }
-         //去重
+        //去重
         List<LogResult> newList=new ArrayList<>();
         for (String temp:tempMap.keySet()) {
             newList.add(tempMap.get(temp));
@@ -212,7 +243,7 @@ class DataHandle {
         return sortList;
 	}
 	
-	//排序
+	//按省份排序
 	public List<LogResult> ListSort (List<LogResult> list) {
 		Collections.sort(list,new Comparator<LogResult>() {
 			public int compare(LogResult r1,LogResult r2) {
@@ -228,10 +259,11 @@ class DataHandle {
 	abstract class Handler {
 		protected Pattern ptn;  //正则表达式
 	    protected Handler nextHandler;  // 下一个处理者
-	 
+	    
 	    public abstract LogResult getData(String str); // 提取数据
 	}
 
+	//新增感染
 	class AddIp extends Handler {
 
 		public AddIp(Pattern p) {
@@ -256,6 +288,7 @@ class DataHandle {
 		
 	}
 
+	//新增疑似
 	class AddSp extends Handler {
 
 		public AddSp(Pattern p) {
@@ -280,6 +313,7 @@ class DataHandle {
 		}
 	}
 	
+	//感染流入
 	class AddAndSubIp extends Handler {
 		public AddAndSubIp(Pattern p) {
 			ptn=p;
@@ -308,6 +342,7 @@ class DataHandle {
 		}
 	}
 		
+	//疑似流入
 	class AddAndSubSp extends Handler {
 		public AddAndSubSp(Pattern p) {
 			ptn=p;
@@ -336,6 +371,7 @@ class DataHandle {
 		}
 	}
 	
+	//死亡
 	class AddDead extends Handler {
 
 		public AddDead(Pattern p) {
@@ -364,6 +400,7 @@ class DataHandle {
 		}	
 	}
 
+	//治愈
 	class AddCure extends Handler {
 
 		public AddCure(Pattern p) {
@@ -392,6 +429,7 @@ class DataHandle {
 		}		
 	}
 
+	//疑似确诊
 	class SpToIp extends Handler {
 
 		public SpToIp(Pattern p) {
@@ -420,6 +458,7 @@ class DataHandle {
 		}		
 	}
 
+	//排除疑似
 	class SubSp extends Handler {
 
 		public SubSp(Pattern p) {
