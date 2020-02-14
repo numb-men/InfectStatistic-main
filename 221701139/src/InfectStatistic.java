8c81e8db569fe01d05c0916a62406fb82783e4db
cunkoulaocai <30389241+cunkoulaocai@users.noreply.github.com>
2020-02-14 19:47:28 +0800

    主文件

--- 221701139/src/InfectStatistic.java
+++ 221701139/src/InfectStatistic.java
@@ -1,8 +1,11 @@
 import argument.ArgParser;
+import command.Command;
+import command.CommandReceiver;
+import command.ListCommand;
 import reg.AllInformation;
 
 import java.io.*;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,38 +15,50 @@ import java.util.regex.Pattern;
  */
 public class InfectStatistic {
     public static void main(String[] args) throws IOException {
-        String REGEX = "(\\S+) 新增 感染患者 (\\d+)人";
-        String INPUT = "福建 新增 感染患者 2";
+        CommandReceiver commandReceiver = new CommandReceiver();
+        Command list = new ListCommand(args,commandReceiver);
+        list.execute();
+        System.out.println("执行完成");
 
-        // 返回一个正则表达式对象
-        Pattern pattern = Pattern.compile(REGEX);
-        // 放回一个匹配器对象
-        Matcher matcher = pattern.matcher(INPUT);
-        boolean b = matcher.matches();
 
-        System.out.println(b);
-        for (String arg:args) {
-            System.out.println(arg);
-        }
-        Map<String, String> arg = ArgParser.argumentParser(args);
-
-        // 解析根据参数执行命令
-        String path = arg.get("-log");
-        // 得到输出文件目录
-        String outFileName = arg.get("-out");
-        File dir = new File(path);
-
-        File[] files = dir.listFiles();
-        for (File file:files) {
-            String filePath = path+"/"+file.getName();
-
-            AllInformation allInformation = new AllInformation();
-            allInformation.processInfo(filePath);
-            allInformation.writeIntoLog("D:/out.txt");
-
-            allInformation.printInfo();
-            // 只测试一个文件
-            break;
-        }
+//        String REGEX = "(\\S+) 新增 感染患者 (\\d+)人";
+//        String INPUT = "福建 新增 感染患者 2";
+//
+//        // 返回一个正则表达式对象
+//        Pattern pattern = Pattern.compile(REGEX);
+//        // 放回一个匹配器对象
+//        Matcher matcher = pattern.matcher(INPUT);
+//        boolean b = matcher.matches();
+//
+//        System.out.println(b);
+//        for (String arg:args) {
+//            System.out.println(arg);
+//        }
+//        Map<String, String> arg = ArgParser.argumentParser(args);
+//
+//        // 解析根据参数执行命令
+//        String path = arg.get("-log");
+//        // 得到输出文件目录
+//        String outFileName = arg.get("-out");
+//        // 得到要处理的日期
+//        String date = arg.get("-date");
+//
+//        File dir = new File(path);
+//
+//        File[] files = dir.listFiles();
+//        for (File file:files) {
+//            if (file.getName().contains(date)) {
+//                String filePath = path+"/"+file.getName();
+//
+//                AllInformation allInformation = new AllInformation();
+//                allInformation.processInfo(filePath);
+//                allInformation.writeIntoLog(outFileName);
+//
+//                allInformation.printInfo();
+//                // 只测试一个文件
+//                break;
+//            }
+//
+//        }
     }
 }
