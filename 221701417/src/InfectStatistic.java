de9ea09d04feb87847cfa4cdee8ef72531ea6456
lzy6666189 <60639011+lzy6666189@users.noreply.github.com>
2020-02-14 16:00:36 +0800

    第02次
    update public static ArrayList<String> getText(File file)

--- 221701417/src/InfectStatistic.java
+++ 221701417/src/InfectStatistic.java
@@ -11,6 +11,27 @@ import java.util.ArrayList;
  */
 class InfectStatistic {
 	
+	public static ArrayList<String> getText(File file) {
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			BufferedReader bReader = new BufferedReader(new FileReader(file));
+			String s = null;
+			while ((s = bReader.readLine()) != null) {
+				if (s.substring(0, 2).equals("//")) {
+					continue;
+				} else {
+					if (!s.equals("")) {
+						list.add(s);
+					}
+				}
+			}
+			bReader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("helloworld");
 	}
