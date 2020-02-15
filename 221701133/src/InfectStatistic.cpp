029a7a69b82dd6656a5fcd655c51f5cee9afd423
17lhf <60611483+17lhf@users.noreply.github.com>
2020-02-16 01:08:26 +0800

    Update InfectStatistic.cpp
    增加函数依据功能细分，增加对命令行参数的处理与检验。删除原来设计的输出运算符重载，改为函数实现类的输出，以保证可选择性多样化输出。

--- 221701133/src/InfectStatistic.cpp
+++ 221701133/src/InfectStatistic.cpp
@@ -4,12 +4,14 @@
 */
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-const string REGIONNAME[] = { "全国","安徽","北京","重庆","福建","甘肃","广东","广西","贵州","海南",
-"河北","河南","黑龙江","湖北","湖南","吉林","江苏","江西","辽宁","内蒙古","宁夏","青海","山东","山西",
-"陕西","上海","四川","天津","西藏","新疆","云南","浙江" };//用于记录时关于地区的顺序模板
+const string REGIONNAME[] = { "全国","安徽","北京","重庆","福建","甘肃","广东",
+"广西","贵州","海南","河北","河南","黑龙江","湖北","湖南","吉林","江苏","江西",
+"辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海","四川","天津","西藏",
+"新疆","云南","浙江"};//用于记录时关于地区的顺序模板
 const int REGIONNUM = sizeof(REGIONNAME) / sizeof(REGIONNAME[0]);//地区数
 
 class Regions //地区类
@@ -27,7 +29,7 @@ public:
 	bool suspectPeopleAdd(const int &n);//增加疑似人数
 	bool curePeopleAdd(const int &n);//增加治愈人数
 	bool deathPeopleAdd(const int &n);//增加死亡人数
-	friend ostream &operator<<(ostream &out,const Regions &r);//用以输出该地区的疫情信息
+	bool selectiveOutput(const string *s);//选择性输出 
 };
 
 class StatisticList //疫情统计表
@@ -35,8 +37,8 @@ class StatisticList //疫情统计表
 	Regions regionsList[REGIONNUM];//地区情况统计表
 public:
 	StatisticList();
+	bool selectiveOutput(const string *s);//选择性输出 
 	friend bool readLogMessage(StatisticList &slist);//读取日志文件并存储
-	friend ostream &operator<<(ostream &out, const StatisticList &s);//用以输出地区列表的疫情信息
 };
 
 Regions::Regions(string n, int c, int s, int cue, int d)
@@ -114,6 +116,11 @@ bool Regions::deathPeopleAdd(const int &n)//增加死亡人数
 	}
 }
 
+bool Regions::selectiveOutput(const string *s)//选择性输出 
+{
+	
+} 
+	
 StatisticList::StatisticList()
 {
 	for (int i = 0; i < REGIONNUM; i++) //默认初始化为各个省份的所有人数参数为零
@@ -122,30 +129,105 @@ StatisticList::StatisticList()
 	}
 }
 
-ostream &operator<<(ostream &out, const Regions &r)//用以输出该地区的疫情信息
+bool StatisticList::selectiveOutput(const string *s)//选择性输出 
 {
-	out << r.name<<" 感染患者"<<r.confirmed<<"人 疑似患者"<<r.suspect<<"人 治"
-		"愈"<<r.cure<<"人 死亡"<<r.death<<"人\n";
-	return out;
+	
 }
 
-ostream &operator<<(ostream &out, const StatisticList &s)//用以输出地区列表的疫情信息
-{
-	for (int i = 0; i < REGIONNUM; i++) //默认初始化为各个省份的所有人数参数为零
+bool isSuportComand(string c){//判断使用的命令是否合理 
+	if(c=="list") 
 	{
-		out << s.regionsList[i];
+		return true;
 	}
-	return out;
+	else
+	{
+		cout<<"暂时只支持list命令\n";
+		return false;
+	}
+}
+
+bool dealParameters(int ac, char* arv[], ofstream &out)//处理传入的命令行参数 
+{
+	int statistic[]={0,0,0,0,0};//为零表示传入的命令行参数没有涉及对应参数设置 
+	for(int i=2;i<ac-1;i+=2)
+	{
+		if(arv[i]=="-log")
+		{
+			statistic[0]=1;
+			dealLogParameters(argv[i+1]);
+		}
+		else if(arv[i]=="-date")
+		{
+			statistic[1]=1;
+			dealDateParameters(argv[i+1]);
+		}
+		else if(arv[i]=="-type")
+		{
+			statistic[2]=1;
+			dealTypeParameters(argv[i+1]);
+		}
+		else if(arv[i]=="-out")
+		{
+			statistic[3]=1;
+			
+		}
+		else if(arv[i]=="-province")
+		{
+			statistic[4]=1;
+			
+		}
+		else
+		{
+			cout<<"传入命令有误！\n"
+			return false;
+		}
+	}
+}
+
+bool dealLogParameters(string p)//处理log的参数
+{
+	
+}
+
+bool dealDateParameters(string p)//处理date的参数
+{
+	
+}
+
+int dealTypeParameters(string p)//处理type的参数
+{
+	
+}
+
+bool dealProvinceParameters(string p)//处理province的参数
+{
+	
+}
+
+bool isRightTimeLog(const string &s)//判断日志是否为要读取的
+{
+
 }
 
 bool readLogMessage(StatisticList &slist)//读取日志文件并存储
 {
+	
 	return true;
 }
 
 int main(int argc, char* argv[])
 {
-
-	std::cout << "Hello World!\n";
+	if(argc>1)//有输入参数时才检查 
+	{
+		if(isSuportComand(argv[1]))//只有支持的命令才会执行操作 
+		{
+			dealParameters(string p);
+		}
+	}
+	else
+	{
+		cout<<"未输入参数！\n";	
+	}
+	cout<<"over\n"; 
 	return 0;
 }
