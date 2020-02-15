802c25d8a36dff61118f5455353bd4bfacfd8fc8
lzy6666189 <60639011+lzy6666189@users.noreply.github.com>
2020-02-16 02:01:56 +0800

    第04次
    update public static void writeTxt(String txtPath,String content)

--- 221701417/src/InfectStatistic.java
+++ 221701417/src/InfectStatistic.java
@@ -1,16 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 /**
- * InfectStatistic
- * TODO
+ * InfectStatistic TODO
  *
  * @author xxx
  * @version xxx
  * @since xxx
  */
 class InfectStatistic {
-	
+
 	public static ArrayList<String> getText(File file) {
 		ArrayList<String> list = new ArrayList<String>();
 		try {
@@ -31,7 +42,7 @@ class InfectStatistic {
 		}
 		return list;
 	}
-	
+
 	public static ArrayList<String> getFiles(String path) {
 		ArrayList<String> files = new ArrayList<String>();
 		File file = new File(path);
@@ -49,8 +60,23 @@ class InfectStatistic {
 		return files;
 	}
 
+//	写出文件
+	public static void writeTxt(String txtPath,String content){    
+	       FileOutputStream fileOutputStream = null;
+	       File file = new File(txtPath);
+	       try {
+	           if(file.exists()){
+	               //判断文件是否存在，如果不存在就新建一个txt
+	               file.createNewFile();
+	           }
+	           fileOutputStream = new FileOutputStream(file);
+	           fileOutputStream.write(content.getBytes());
+	           fileOutputStream.flush();
+	           fileOutputStream.close();
+	       } catch (Exception e) {
+	           e.printStackTrace();
+	       }
+	    }
 	
-	public static void main(String[] args) {
-		System.out.println("helloworld");
-	}
-} 
+	
+}
