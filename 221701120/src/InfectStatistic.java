6252bcccf9908f635bcd10cbcf8ab2ac5b592265
SupremacyXXXXX <741976368@qq.com>
2020-02-16 01:07:00 +0800

    feat
    add class InfectedArea

--- 221701120/src/InfectStatistic.java
+++ 221701120/src/InfectStatistic.java
@@ -26,32 +26,6 @@ class InfectStatistic {
 
 /**
  * 
- * 文件读取的工具类
- * TODO
- *
- * @author 221701120_hxy
- * @version 1.0
- * @since 2020.2.15
- */
-class FileUtils{
-    
-    public static void readToBuffer(StringBuffer buffer, String filePath) throws IOException {
-        InputStream inStream = new FileInputStream(filePath);
-        String line; // 用来保存每行读取的内容
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
-        line = reader.readLine(); // 读取第一行
-        while (line != null) { // 如果 line 为空说明读完了
-            buffer.append(line); // 将读到的内容添加到 buffer 中
-            buffer.append("\n"); // 添加换行符
-            line = reader.readLine(); // 读取下一行
-        }
-        reader.close();
-        inStream.close();
-    }
-}
-
-/**
- * 
  * 用来解析命令行传入的参数
  * TODO
  * 
@@ -92,11 +66,50 @@ class CommandParser{
         }
     }
     
-    static String getContent() {
-        String srcPath = "";
-        
-        return srcPath;
+    
+}
+
+/**
+ * 
+ * 文件读取的工具类
+ * TODO
+ *
+ * @author 221701120_hxy
+ * @version 1.0
+ * @since 2020.2.15
+ */
+class FileUtils{
+    
+    public static void readToBuffer(StringBuffer buffer, String filePath) throws IOException {
+        InputStream inStream = new FileInputStream(filePath);
+        String line; // 用来保存每行读取的内容
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+        line = reader.readLine(); // 读取第一行
+        while (line != null) { // 如果 line 为空说明读完了
+            buffer.append(line); // 将读到的内容添加到 buffer 中
+            buffer.append("\n"); // 添加换行符
+            line = reader.readLine(); // 读取下一行
+        }
+        reader.close();
+        inStream.close();
     }
+}
+
+/**
+ * 
+ * 存储被感染省份地区的相关信息
+ * TODO
+ *
+ * @author 221701120_hxy
+ * @version 1.0
+ * @since 2020.2.15
+ */
+class InfectedArea{
+    private String areaName;
+    private int infectedNum = 0;
+    private int potentialNum = 0;
+    private int curedNum = 0;
+    private int deadNum = 0;
     
 }
 
