785df656e5b48918a96e1e45242ddd13a7c58d44
HNT <1090687607@qq.com>
2020-02-15 12:55:06 +0800

    Update codestyle
    更新代码规范

---
+++ 221701201/src/InfectStatistic.java
@@ -0,0 +1,18 @@
+/**
+ * InfectStatistic
+ * TODO
+ *
+ * @author 221701201
+ * @version 1.0.0
+ * @since 2020.02.13
+ */
+
+class InfectStatistic {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
+
+
+
+
