50ae58e245e71cc3a88fdc15119adc0b802eba93
hyh990511 <53004798+hyh990511@users.noreply.github.com>
2020-02-15 11:40:38 +0800

    Update InfectStatistic.java

--- 221701226/src/InfectStatistic.java
+++ 221701226/src/InfectStatistic.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -45,12 +49,13 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		CmdArgsParse  CmdArgs = new  InfectStatistic().new CmdArgsParse();
 		CmdArgs.isCurrentArgs(argsStr); 
 		FileDispose filedispose = new InfectStatistic().new FileDispose();
+		filedispose.readFlieList();
 		
 	}		
 	
-	class CmdArgsParse{  //命令行解析类，主要对命令行参数进行解析
+	class CmdArgsParse {  //命令行解析类，主要对命令行参数进行解析
 		
-		CmdArgsParse(){}  //空构造函数
+		CmdArgsParse() {}  //空构造函数
 		public  boolean isCurrentArgs(String[] args) {  //判断该命令行是否为有效的命令行并且解析出每个部分相应的参数
 		
 		boolean isLogExist = false;  //因为-log为必须附带，所以需要进行是否有-log存在的判断
@@ -60,9 +65,9 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		        System.out.println("命令行有误");
 		        return false;
 		    }
-		for (int i=1; i<args.length; i++){  //将各部分的内容一一判断并且解析参数,解析有误则停止解析并输出命令行有误
+		for (int i=1; i<args.length; i++) {  //将各部分的内容一一判断并且解析参数,解析有误则停止解析并输出命令行有误
 			
-			if(args[i].equals("-log")){  //-log部分的解析
+			if(args[i].equals("-log")) {  //-log部分的解析
 				i++;  //将命令行参数移动到准备解析的部分
 				isLogExist = true ;  //-log存在的判断
 				if(getLogPath(i, args) == -1) {  //获得日志文件所在地址存在错误
@@ -71,7 +76,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 				}
 			}
 			
-			if(args[i].equals("-out")){  //-out部分的解析
+			if(args[i].equals("-out")) {  //-out部分的解析
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getOutPath(i, args) == -1) {  //获得日志文件所在地址存在错误
 					System.out.println("命令行有误");
@@ -79,7 +84,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 				}
 			}
 			
-			if(args[i].equals("-date")){  //-date部分的解析
+			if(args[i].equals("-date")) {  //-date部分的解析
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getDate(i, args) == -1) {  //获得日志文件所在地址存在错误
 					System.out.println("命令行有误");
@@ -87,7 +92,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 				}
 			}
 			
-			if(args[i].equals("-type")){  //-type部分的解析
+			if(args[i].equals("-type")) {  //-type部分的解析
 				isTypeExist = true;  //-type部分有指定
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getType(i, args) == -1) {  //获得日志文件所在地址存在错误
@@ -96,7 +101,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 				}
 			}
 			
-			if(args[i].equals("-province")){  //-province部分的解析
+			if(args[i].equals("-province")) {  //-province部分的解析
 				isProvinceExist = true;  //-province部分有指定
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getProvince(i, args) == -1) {  //获得日志文件所在地址存在错误
@@ -114,7 +119,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 
 
 	
-	 public int getLogPath(int i, String[] args){  //获得日志文件所在地址
+	 public int getLogPath(int i, String[] args) {  //获得日志文件所在地址
 		if(i < args.length) {  //当下标未越界
 	        if(args[i].matches("^[A-z]:\\\\(.+?\\\\)*$"))  //通过正则表达式来判断字符串是不是文件目录路径
 	        	logPath = args[i];
@@ -136,7 +141,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		return i;  		
 	 }
 	
-	 public  int getDate(int i, String[] args){  //获得指定日期
+	 public  int getDate(int i, String[] args) {  //获得指定日期
 		if(i < args.length) {  //当下标未越界
 			if(date.compareTo(args[i]) >= 0)  //判断指定日期是否超过当前日期
 				date = args[i];   //若不超过，将日期更改为指定日期
@@ -147,7 +152,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		return i;  
 	 }
 	
-     public int getType(int i, String[] args){  //获得指定类型
+     public int getType(int i, String[] args) {  //获得指定类型
     	 int j;
     	if(i < args.length) {  //当下标未越界
     		 for(j = 0; j < 4; j++)  //类型判断输出数组置0
@@ -181,7 +186,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		return i;  
 	 }
     
-     public int getProvince(int i, String[] args){  //获得指定省份
+     public int getProvince(int i, String[] args) {  //获得指定省份
     	 int j;
     	 if(i < args.length) {  //当下标未越界
     		 for(j = 0; j < 35; j++)  //判断省份输出数组置0
@@ -212,11 +217,43 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 		
 		FileDispose(){};  //空构造函数
 		
+		public void readFlieList() {  //读取指定路径下的文件名
+			date = date + ".log.txt";  //把date加上后缀
+			File file = new File(logPath);
+			String[] files = file.list();  //list()方法是返回某个目录下的所有文件和目录的文件名，返回的是String数组
+			
+			for(int j = 0; j < files.length; j++) {  
+				if(files[j].compareTo(date) <= 0) {  //判断该文件时间是否小于指定时间
+					readLogTxt(logPath + files[j]);  //开始读取日志文件内容
+				}
+			}
+			
+		}
+		
+		public void readLogTxt(String filePath){ //读取日志文件内容
+			 try {
+			        BufferedReader bfr = new BufferedReader
+			        	(new InputStreamReader(new FileInputStream(new File(filePath)),"UTF-8")); //通过普通的缓存方式文本读取，编码为UTF-8
+			        String readLine = null;
+			        while ((readLine = bfr.readLine()) != null) {  //按行读取文本内容
+			            if(! readLine.startsWith("//"))  //遇到“//”不读取
+			                handleReadLine(readLine);  //开始进行文本内容的处理
+			        }
+			        bfr.close();  //关闭文件
+			 }  catch (Exception e) {  //异常处理
+			        e.printStackTrace();
+			    }
+			
+		} 
+		
+		
+		public  void  handleReadLine(String readLine){  //处理文本内容的信息
+			
+			
+		}
 	}
 	
 	
-
-
 	}
 
 
