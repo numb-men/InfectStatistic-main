ad8a407f5be10c672e0a69abc0bb90bbcd27d5fd
hyh990511 <53004798+hyh990511@users.noreply.github.com>
2020-02-15 10:10:24 +0800

    完成CmdArgsParse类(命令行参数解析类)

--- 221701226/src/InfectStatistic.java
+++ 221701226/src/InfectStatistic.java
@@ -18,27 +18,34 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 	String logPath;  //日志文件所在地址
 	String outPath;  //输出文件所在地址
 	
-	//因为-date不设置则默认为所提供日志最新的一天，提前设置时间为当前时间
+	//因为-date不设置则指定日期默认为所提供日志最新的一天，需要确保你处理了指定日期之前的所有log文件，所以提前设置时间为当前时间
 	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); 
 	Date d = new Date(System.currentTimeMillis());
-	public String date = formatter.format(d);//当前时间设置
+	String date = formatter.format(d);//当前时间设置
 	
-	//因为-type不指定默认会列出所有情况，所以创建数组来表示类型的情况
-	int[] type = {1,2,3,4};  //类型的数组，应用于获得类型方法
-	public String[] typeStr = {"感染患者", "疑似患者", "治愈", "死亡"};  //对应于类型数组的类型顺序排列
+	//设置-type下的信息
+	int[] type; //类型输出判断数组，初始默认为0，序号1代表感染患者，序号2代表疑似患者，序号3代表治愈，序号4代表死亡
+	boolean  isTypeExist = false;  //判断-Type是否有指定
+	public String[] typeStr = {"", "感染患者", "疑似患者", "治愈", "死亡"};  //类型顺序排列
 	
 	//设置-province下的信息
-	public int[] province = new int[35];  //省份是否输出的判断数组，将值为1的省份输出
+	boolean[] province;  //若有指定省份，用来判断省份是否输出数组,初始默认为0
+	boolean  isProvinceExist = false;  //判断-Province是否有指定 
 	public String[] provinceStr = {"全国", "安徽", "澳门" ,"北京", "重庆", "福建","甘肃","广东", "广西", "贵州", "海南", "河北", "河南", "黑龙江", "湖北", "湖南", 
 			"吉林","江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东", "山西", "陕西", "上海","四川", "台湾", "天津", "西藏", "香港", "新疆", "云南", "浙江"};
 	//全国以及省份顺序排列
 	
 	public int[][] peopleNumber = new int [35][4];  //记录全国以及每个省份每个类型的人数，初始默认为0，按照上面类型和省份顺序排列
 	
+	
+	
 	public static  void main(String[] args) {  //主函数入口
+		
 		String[] argsStr=args;  //将命令行传递到字符串函数
 		CmdArgsParse  CmdArgs = new  InfectStatistic().new CmdArgsParse();
 		CmdArgs.isCurrentArgs(argsStr); 
+		FileDispose filedispose = new InfectStatistic().new FileDispose();
+		
 	}		
 	
 	class CmdArgsParse{  //命令行解析类，主要对命令行参数进行解析
@@ -81,6 +88,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 			}
 			
 			if(args[i].equals("-type")){  //-type部分的解析
+				isTypeExist = true;  //-type部分有指定
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getType(i, args) == -1) {  //获得日志文件所在地址存在错误
 					System.out.println("命令行有误");
@@ -89,6 +97,7 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 			}
 			
 			if(args[i].equals("-province")){  //-province部分的解析
+				isProvinceExist = true;  //-province部分有指定
 				i++;  //将命令行参数移动到准备解析的部分
 				if(getProvince(i, args) == -1) {  //获得日志文件所在地址存在错误
 					System.out.println("命令行有误");
@@ -105,45 +114,95 @@ class InfectStatistic { //主类有内部类FileDispose(文件处理类)，CmdAr
 
 
 	
-	public int getLogPath(int i, String[] args){//获得日志文件所在地址
-		if(i < args.length) { //当下标未越界
-	        if(args[i].matches("^[A-z]:\\\\(.+?\\\\)*$")) //通过正则表达式来判断字符串是不是文件目录路径
+	 public int getLogPath(int i, String[] args){  //获得日志文件所在地址
+		if(i < args.length) {  //当下标未越界
+	        if(args[i].matches("^[A-z]:\\\\(.+?\\\\)*$"))  //通过正则表达式来判断字符串是不是文件目录路径
 	        	logPath = args[i];
 	        else  
-	            return -1;
+	          return -1;
 	    } else
 	        return -1;
 	    return i;  
-	}
+	 }
 	
-	public int getOutPath(int i, String[] args){//获得输出文件所在地址
-		if(i < args.length) { //当下标未越界
-	        if(args[i].matches("^[A-z]:\\\\(\\S+)+(\\.txt)$")) //通过正则表达式来判断字符串是不是txt文件路径
+	 public int getOutPath(int i, String[] args){  //获得输出文件所在地址
+		if(i < args.length) {  //当下标未越界
+	        if(args[i].matches("^[A-z]:\\\\(\\S+)+(\\.txt)$"))  //通过正则表达式来判断字符串是不是txt文件路径
 	            outPath = args[i];
 	        else  
-	            return -1;
+	          return -1;
 	    } else
 	        return -1;
 		return i;  		
-	}
+	 }
 	
-	public  int getDate(int i, String[] args){//获得指定日期
-		
-		
+	 public  int getDate(int i, String[] args){  //获得指定日期
+		if(i < args.length) {  //当下标未越界
+			if(date.compareTo(args[i]) >= 0)  //判断指定日期是否超过当前日期
+				date = args[i];   //若不超过，将日期更改为指定日期
+			else 
+			  return -1;
+		} else
+		    return -1;
 		return i;  
-	}
+	 }
 	
-    public int getType(int i, String[] args){//获得指定类型
-    	
-    	
+     public int getType(int i, String[] args){  //获得指定类型
+    	 int j;
+    	if(i < args.length) {  //当下标未越界
+    		 for(j = 0; j < 4; j++)  //类型判断输出数组置0
+    	            type[j] = 0;
+    		 j=0;
+    		 while(i < args.length){  //开始判断指定类型并且获得输出顺序
+    			if(args[i].equals("ip")){
+    				type[j] = 1;
+    				j++;
+    				i++;
+    			} 
+    			if(args[i].equals("sp")){
+    				type[j] = 2;
+    				j++;
+    				i++;
+    			} 
+    			if(args[i].equals("cure")){
+    				type[j] = 3;
+    				j++;
+    				i++;
+    			} 
+    			if(args[i].equals("dead")){
+    				type[j] = 4;
+    				j++;
+    				i++;
+    			} 
+    		 }
+    	} else
+    		return -1;
+    	i--;	
 		return i;  
-	}
+	 }
     
-    public int getProvince(int i, String[] args){//获得指定省份
-    	
-    	
+     public int getProvince(int i, String[] args){  //获得指定省份
+    	 int j;
+    	 if(i < args.length) {  //当下标未越界
+    		 for(j = 0; j < 35; j++)  //判断省份输出数组置0
+    			 province[j] = false;
+    		 while(i < args.length){
+    			 for(j = 0; j < 35; j++){
+    				 if(args[i].equals(provinceStr[j])){
+    					 province[j] = true;
+    					 i++;
+    					 break;
+    				 }
+    			 }
+    			 
+    		 }
+    	 } else
+    		 return -1;
+    	i--;
 		return i; 		
-	}}
+	 }
+    
+	}
 	
 	
 	
