20c210393f0f77b16a888629a3c54497a76bdf20
DDDDy <2722537573@qq.com>
2020-02-16 01:57:31 +0800

    测试读取命令参数进行的文件读写
    通过固定的命令来测试文件读写功能，方便功能完善，避免发生处理参数功能更完成后文件读写功能却没有实现。

--- 221701235/src/InfectStatistic.java
+++ 221701235/src/InfectStatistic.java
@@ -2,12 +2,53 @@
  * InfectStatistic
  * TODO
  *
- * @author xxx
- * @version xxx
- * @since xxx
+ * @author DDDDy
+ * @version 1.0
+ * @since 2020-02-16
  */
-class InfectStatistic {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class InfectStatistic {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        //java InfectStatistic list -date 2020-01-22 -log D:/log/ -out D:/output.txt
+        //按照上述命令（已知）测试文件读写
+        System.out.println("命令测试：");
+        for (int i = 0;i < args.length;i++) {
+            System.out.println(args[i]);
+        }
+        String dateInput = args[2];
+        String logPath = args[4];
+        String outPath = args[6];
+        String filePath = logPath + "/" +dateInput + ".log.txt";
+        String fileContent = "";
+        
+        //读取
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"UTF-8");
+            BufferedReader buffReader = new BufferedReader(inputStreamReader);
+            String line = null;   
+            while ((line = buffReader.readLine()) != null) {
+                fileContent += line;
+                fileContent +="\n";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        //写入
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(outPath);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream,"UTF-8");
+            outputStreamWriter.write(fileContent);
+            outputStreamWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
