a3021b350a38766b905977d8afe2d0e6bb34ed1e
ReckyZzz <46258043+ReckyZzz@users.noreply.github.com>
2020-02-16 19:18:33 +0800

    Update InfectStatistic.java
    基本完成文件读取功能的实现

--- example/src/InfectStatistic.java
+++ example/src/InfectStatistic.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  * InfectStatistic
  * TODO
@@ -20,16 +22,23 @@ class FileManager{
     public String[] provinces={"安徽","北京","重庆","福建","甘肃","广东","广西","贵州",
             "海南","河北","河南","黑龙江","湖北","湖南","吉林","江苏","江西","辽宁","内蒙古",
             "宁夏","青海","山东","山西","陕西","上海","四川","天津","西藏","新疆","云南","浙江"};
+    public int[] InfectedPatients= new int[31];
+    public int[] SuspectedPatients=new int[31];
+    public int[] CurePatients=new int[31];
+    public int[] DeadPatients=new int[31];
+    public int AllIP=0,AllSP=0,AllCP=0,AllDP=0;
+    private static String REGEX_CHINESE = "[\u4e00-\u9fa5]";// 中文正则
+    ArrayList<String> fileNames = new ArrayList<>();
+    ArrayList<String> fileContent = new ArrayList<>();
 
     public FileManager(){
 
     }
     public void ReadFile(String FilePath,String dateparam){
         File f = new File(FilePath);
-        int stopPoint=0;
+        int stopPoint=0;//判断读取哪些文件的断点
+        int flag=0;//判断日期是否合法
         String stopDate = dateparam + ".log.txt";
-        ArrayList<String> fileNames = new ArrayList<>();
-        ArrayList<String> fileContent = new ArrayList<>();
         if(f.isDirectory()) {
             String[] temp = f.list();
             for(int i=0;i<temp.length;i++)
@@ -42,12 +51,15 @@ class FileManager{
             stopPoint=fileNames.size();
         else{
             for(int i=0;i<fileNames.size();i++){
-                if(fileNames.get(i).compareTo(stopDate) <= 0){
+                if(fileNames.get(i).compareTo(stopDate) == 0){
                     stopPoint=i;
+                    flag=1;
                 }
-                /*System.out.printf("此时%s与%s的比较结果为：%d\n",fileNames.get(i),stopDate
-                        ,fileNames.get(i).compareTo(stopDate));*/
             }
+        }
+        if(flag==0){
+            System.out.println("输入的日期超出范围！");
+            return;
         }
         try {
             for(int i=0;i<=stopPoint;i++) {
@@ -55,15 +67,106 @@ class FileManager{
                 BufferedReader br = new BufferedReader(new InputStreamReader(
                         new FileInputStream(SingleFile),"UTF-8"));
                 String line = "";
-                while((line=br.readLine())!=null)
-                    fileContent.add(line);
+                while((line=br.readLine())!=null) {
+                    if(!line.startsWith("/"))
+                        fileContent.add(line);
+                }
             }
         }catch (Exception e){
 
         }
+        GetStatistic(fileContent);
     }
-    public void GetStatistic(){
 
+    public void GetStatistic(ArrayList<String> fileContent){
+        for(int i=0;i<fileContent.size();i++){
+            String[] temp=fileContent.get(i).split(" ");
+            DealStrings(temp);
+        }
+        System.out.printf("全国 感染患者%d人 疑似患者%d人 治愈%d人 死亡%d人\n",
+                AllIP,AllSP,AllCP,AllDP);
+        for(int i=0;i<31;i++){
+            System.out.printf("%s 感染患者%d人 疑似患者%d人 治愈%d人 死亡%d人\n",
+                    provinces[i],InfectedPatients[i],SuspectedPatients[i],
+                    CurePatients[i],DeadPatients[i]);
+        }
+    }
+
+    public void DealStrings(String[] str){
+        int index = 0;
+        int increase;
+        String tes=str[str.length-1];
+        Pattern pat = Pattern.compile(REGEX_CHINESE);
+        Matcher mat = pat.matcher(tes);
+        increase=Integer.parseInt(mat.replaceAll(""));
+        for(int i=0;i<provinces.length;i++){
+            if(provinces[i].equals(str[0])){
+                index=i;
+                break;
+            }
+        }
+        if(str[1].equals("新增")){
+            if(str[2].equals("感染患者")){
+                InfectedPatients[index]+=increase;
+                AllIP+=increase;
+                return;
+            }
+            else if(str[2].equals("疑似患者")){
+                SuspectedPatients[index]+=increase;
+                AllSP+=increase;
+                return;
+            }
+        }
+        else if(str[1].equals("感染患者")){
+            InfectedPatients[index]-=increase;
+            for(int i=0;i<provinces.length;i++){
+                if(provinces[i].equals(str[3])){
+                    index=i;
+                    break;
+                }
+            }
+            InfectedPatients[index]+=increase;
+            return;
+        }
+        else if(str[1].equals("疑似患者")){
+            if(str[2].equals("流入")){
+                SuspectedPatients[index]-=increase;
+                for(int i=0;i<provinces.length;i++){
+                    if(provinces[i].equals(str[3])){
+                        index=i;
+                        break;
+                    }
+                }
+                SuspectedPatients[index]+=increase;
+                return;
+            }
+            else if(str[2].equals("确诊感染")){
+                SuspectedPatients[index]-=increase;
+                InfectedPatients[index]+=increase;
+                AllSP-=increase;
+                AllIP+=increase;
+                return;
+            }
+        }
+        else if(str[1].equals("死亡")){
+            InfectedPatients[index]-=increase;
+            DeadPatients[index]+=increase;
+            AllIP-=increase;
+            AllDP+=increase;
+            return;
+        }
+        else if(str[1].equals("治愈")){
+            InfectedPatients[index]-=increase;
+            CurePatients[index]+=increase;
+            AllIP-=increase;
+            AllCP+=increase;
+            return;
+        }
+        else if(str[1].equals("排除")){
+            SuspectedPatients[index]-=increase;
+            AllSP-=increase;
+            return;
+        }
     }
 }
 class CommandIdentity{
@@ -140,7 +243,7 @@ class CommandIdentity{
                 else
                     provinceparam.add(command[i]);
             }
-            System.out.printf("-log的位置为：%d  参数为：%s\n",logpos,logparam);
+            /*System.out.printf("-log的位置为：%d  参数为：%s\n",logpos,logparam);
             System.out.printf("-out的位置为：%d  参数为：%s\n",outpos,outparam);
             System.out.printf("-date的位置为：%d  参数为：%s\n",datepos,dateparam);
             System.out.printf("-type的位置为：%d  参数为：",typepos);
@@ -150,7 +253,7 @@ class CommandIdentity{
             System.out.printf("-province的位置为：%d  参数为：",provincepos);
             for(String s:provinceparam)
                 System.out.print(s + " ");
-            System.out.println();
+            System.out.println();*/
         }
         FileManager fm = new FileManager();
         fm.ReadFile(logparam,dateparam);
