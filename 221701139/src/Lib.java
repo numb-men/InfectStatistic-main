03b9c89acee58bcca1530e75af8b2f3ee73f0641
cunkoulaocai <30389241+cunkoulaocai@users.noreply.github.com>
2020-02-16 14:50:10 +0800

    添加正则表达式5678

--- 221701139/src/Lib.java
+++ 221701139/src/Lib.java
@@ -309,6 +309,112 @@ public class Lib {
             return map;
         }
     }
+
+    public class RegFive {
+        private final String REGEX = "(\\S+) 死亡 (\\d+)人";
+
+
+        // 返回一个省份对应信息的映射
+        public Map<String, Integer[]> process(String line) {
+
+            Map<String,Integer[]> map = new HashMap<>();
+
+            String[] result = line.split(" ");
+            //            for (String s:result) {
+            //                System.out.println(s);
+            //            }
+            // 获取省份
+            String province = result[0];
+            // 获取人数
+            Integer population = Integer.parseInt(result[2].substring(0,result[2].length()-1));
+
+            // 封装成map
+            Integer[] num = {-population,0,0,population};
+            map.put(province,num);
+            map.put("全国",new Integer[]{-population,0,0,population});
+            return map;
+        }
+    }
+
+    public class RegSix {
+        private final String REGEX = "(\\S+) 治愈 (\\d+)人";
+
+        public RegSix() {
+
+        }
+
+        // 返回一个省份对应信息的映射
+
+        public Map<String, Integer[]> process(String line) {
+
+            Map<String,Integer[]> map = new HashMap<>();
+
+            String[] result = line.split(" ");
+            //            for (String s:result) {
+            //                System.out.println(s);
+            //            }
+            // 获取省份
+            String province = result[0];
+            // 获取人数
+            Integer population = Integer.parseInt(result[2].substring(0,result[2].length()-1));
+
+            // 封装成map
+            Integer[] num = {-population,0,population,0};
+            map.put(province,num);
+            map.put("全国",new Integer[]{-population,0,population,0});
+            return map;
+        }
+    }
+    public class RegSeven {
+        private final String REGEX = "(\\S+) 疑似患者 确诊感染 (\\d+)人";
+
+        public RegSeven() {
+
+        }
+
+        // 返回一个省份对应信息的映射
+
+        public Map<String, Integer[]> process(String line) {
+
+            Map<String,Integer[]> map = new HashMap<>();
+
+            String[] result = line.split(" ");
+            //            for (String s:result) {
+            //                System.out.println(s);
+            //            }
+            // 获取省份
+            String province = result[0];
+            // 获取人数
+            Integer population = Integer.parseInt(result[3].substring(0,result[3].length()-1));
+
+            // 封装成map
+            Integer[] num = {population,-population,0,0};
+            map.put(province,num);
+            map.put("全国",new Integer[]{population,-population,0,0});
+            return map;
+        }
+    }
+    public class RegEight {
+        private final String REGEX = "(\\S+) 排除 疑似患者 (\\d+)人";
+        // 返回一个省份对应信息的映射
+        public Map<String, Integer[]> process(String line) {
+
+            Map<String,Integer[]> map = new HashMap<>();
+            String[] result = line.split(" ");
+            //            for (String s:result) {
+            //                System.out.println(s);
+            //            }
+            // 获取省份
+            String province = result[0];
+            // 获取人数
+            Integer population = Integer.parseInt(result[3].substring(0,result[3].length()-1));
+            // 封装成map
+            Integer[] num = {0,-population,0,0};
+            map.put(province,num);
+            map.put("全国",new Integer[]{0,-population,0,0});
+            return map;
+        }
+    }
 }
 
 
