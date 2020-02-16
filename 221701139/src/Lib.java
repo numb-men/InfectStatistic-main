9c841d98caee50edd2faf63cfa1b16cdb292467d
cunkoulaocai <30389241+cunkoulaocai@users.noreply.github.com>
2020-02-16 14:47:21 +0800

    添加正则表达式处理1

--- 221701139/src/Lib.java
+++ 221701139/src/Lib.java
@@ -205,7 +205,28 @@ public class Lib {
         }
     }
 
+    public class RegOne {
 
+        // <省> 新增 感染患者 n人
+        private final String REGEX = "(\\S+) 新增 感染患者 (\\d+)人";
+
+        // 返回一个省份对应信息的映射
+        public Map<String, Integer[]> process(String line) {
+            Map<String,Integer[]> map = new HashMap<>();
+            // 如果匹配代表该行由这个正则表达式处理
+            String[] result = line.split(" ");
+            // 获取省份
+            String province = result[0];
+            // 获取人数
+            Integer population = Integer.parseInt(result[3].substring(0,result[3].length()-1));
+
+            // 封装成map
+            Integer[] num = {population,0,0,0};
+            map.put(province,num);
+            map.put("全国",new Integer[]{population,0,0,0});
+            return map;
+        }
+    }
 }
 
 
