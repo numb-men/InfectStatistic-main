7dadf6b61c5681b3c9a865716cb625361c5f4cb0
xuguangqing <60814044+xuguangqing@users.noreply.github.com>
2020-02-16 22:38:27 +0800

    second

--- 221701428/src/InfectStatistic.java
+++ 221701428/src/InfectStatistic.java
@@ -24,6 +24,91 @@ class InfectStatistic {
     /** 用来存储省份的哈希表 */
     public static Hashtable<String, Province> hashtable = new Hashtable<String, Province>(40);
 
+    /** Province类 */
+    public class Province {
+
+        /** 省份名称 */
+        String provinceName;
+        /** 感染患者 */
+        long ip;
+        /** 疑似患者 */
+        long sp;
+        /** 治愈 */
+        long cure;
+        /** 死亡 */
+        long dead;
+
+        Province(String provinceName, int ip, int sp, int cure, int dead) {
+            this.provinceName = provinceName;
+            this.ip = ip;
+            this.sp = sp;
+            this.cure = cure;
+            this.dead = dead;
+        }
+        public void increaseIp(int newIpNum) {
+            ip += newIpNum;
+        }
+        public void decreaseIp(int ipNum) {
+            ip -= ipNum;
+        }
+        public void increaseSp(int newSpNum) {
+            sp += newSpNum;
+        }
+        public void decreaseSp(int spNum) {
+            sp -= spNum;
+        }
+        public void increaseCure(int newCureNum) {
+            cure += newCureNum;
+        }
+        public void increaseDead(int newDeadNum) {
+            dead += newDeadNum;
+        }
+        public String getProvinceName() {
+            return provinceName;
+        }
+        public long getIp() {
+            return ip;
+        }
+        public long getSp() {
+            return sp;
+        }
+        public long getCure() {
+            return cure;
+        }
+        public long getDead() {
+            return dead;
+        }
+        /** description：打印全部统计的数据结果 */
+        public String getDefaultResult() {
+            String resString = provinceName + ' ' + "感染患者" + ip + "人" + ' ' + "疑似患者" + sp + "人" + ' ' + "治愈" + cure
+                    + "人" + ' ' + "死亡" + dead + "人";
+            return resString;
+        }
+        /** description：按指定参数值要求给出结果 */
+        public String getAppointResult(String[] paramentersOfType) {
+            String resString = provinceName + ' ';
+            for(int i=0; paramentersOfType[i] != null; i++) {
+                switch (paramentersOfType[i]) {
+                    case "ip":
+                        resString += "感染患者" + ip + "人" + ' ';
+                        break;
+                    case "sp":
+                        resString += "疑似患者" + sp + "人" + ' ';
+                        break;
+                    case "cure":
+                        resString += "治愈" + cure + "人" + ' ';
+                        break;
+                    case "dead":
+                        resString += "死亡" + dead + "人" + ' ';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return resString;
+        }
+    }
+
     public static void main(String[] args) {
         //...
     }
