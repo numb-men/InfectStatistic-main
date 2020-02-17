2aef96f34df204135f8bd8825fd905233ecbaa88
Hanani-Chen <351346183@qq.com>
2020-02-17 16:02:02 +0800

    初步完成对命令的分解

--- 221701416/src/InfectStatistic.java
+++ 221701416/src/InfectStatistic.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.*;
+
 /**
  * InfectStatistic
  *
@@ -8,21 +9,85 @@ import java.util.*;
  * @since xxx
  */
 class InfectStatistic {
-	public String path=new String();
+	public static String log=new String();
+	public static String out=new String();
+	public static String date=new String("0000-00-00");
+	public static String []type= new String[4];
+	public static String []province=new String[40];
     public static void main(String[] args) throws IOException {
     	CoronavirusDetail CD=new CoronavirusDetail();
     	CD.Init();
     	AnalysisCommand(args);
-    	CD.ReadAll();
+    	CD.ReadAll(log,out,date,type,province);
     }
+    //对输入的命令进行处理分解
     public static void AnalysisCommand(String[] command) {
+    	int len=command.length;
     	String $list= new String("list");
-    	for(int i=0;i<command.length;i++) {
-    		if((i==0) && !(command[i].equals($list))) {
-    			System.out.println("请使用list命令进行操作");
-    			System.exit(0);
-    		}
-    	}
+    	String $log= new String("-log");
+    	String $out= new String("-out");
+    	String $date= new String("-date");
+    	String $type= new String("-type");
+    	String $province= new String("-province");
+    	String $nothing=new String("nothing");
+    	//初始化type和province
+    	for(int i=0;i<4;i++) type[i]=$nothing;
+    	for(int i=0;i<40;i++) province[i]=$nothing;
+    	
+    	//list检查
+		if(!(command[0].equals($list))) {
+			System.out.println("请使用list命令进行操作");
+			System.exit(0);
+		}
+		//获取log目录
+		for(int i=1;i<len;i++) {
+			if(command[i].equals($log)) {
+				log=command[i+1];
+				break;
+			}
+		}
+		//获取out目录
+		for(int i=1;i<len;i++) {
+			if(command[i].equals($out)) {
+				out=command[i+1];
+				break;
+			}
+		}
+		//获取date数值
+		for(int i=1;i<len;i++) {
+			if(command[i].equals($date)) {
+				date=command[i+1];
+				break;
+			}
+		}
+		//获取type的参数
+		int typepos=0;
+		for(int i=1;i<len;i++) {
+			if(command[i].equals($type)) {
+				typepos=i;
+				break;
+			}
+		}
+		int typeindex=0;
+		for(int i=typepos+1;i<len;i++) {
+			if(command[i].charAt(0)!='-') {
+				type[typeindex++]=command[i];
+			}else break;
+		}
+		//获取province的参数
+		int provincepos=0;
+		for(int i=1;i<len;i++) {
+			if(command[i].equals($province)) {
+				provincepos=i;
+				break;
+			}
+		}
+		int provinceindex=0;
+		for(int i=provincepos+1;i<len;i++) {
+			if(command[i].charAt(0)!='-') {
+				province[provinceindex++]=command[i];
+			}else break;
+		}
     }
 }
 /**
@@ -49,7 +114,15 @@ class CoronavirusDetail{
 			ProvinceMap.put(ProvinceStr[i], Integer.valueOf(i));
 		}
 	}
-	public void ReadAll() throws IOException{
-		
+	public void ReadAll(String log,String out,String date,String [] type,String [] province) throws IOException{
+		System.out.println(log);
+		System.out.println(out);
+		System.out.println(date);
+		for(int i=0;i<4;i++) {
+			System.out.println(type[i]);
+		}
+		for(int i=0;i<40;i++) {
+			System.out.println(province[i]);
+		}
 	}
 }
