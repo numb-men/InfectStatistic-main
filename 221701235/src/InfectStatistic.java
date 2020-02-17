aa0dec51575d656f2fbce2ca0cd30e4de6a8cf47
DDDDy <2722537573@qq.com>
2020-02-18 00:38:06 +0800

    读取文件，测试正则表达式
    读取文件文本，匹配正则表达式测试，测试完成后方便给出每种类型的计算方法。

--- 221701235/src/InfectStatistic.java
+++ 221701235/src/InfectStatistic.java
@@ -6,7 +6,11 @@
  * @version 1.0
  * @since 2020-02-16
  */
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class InfectStatistic {
     public static void main(String[] args) {
@@ -122,15 +126,84 @@ public class InfectStatistic {
                 }
                 for (String name : names) {
                     int res = dateInput.compareTo(name);
-                    if (res > 0) {
-                        //当前name的日期比指定日期（未指定则认为指定日志最后一个日期即最新）早
+                    if (res >= 0) {
+                        //当前name的日期比指定日期（未指定则认为指定日志最后一个日期即最新）早或同一天
                         //读取 String filePath = logPath + "/" + name 文件
+                        String filePath = logPath + "/" +name;
                         //处理，统计
-                    } else if (res == 0) {
-                        //当前name处理就可以退出了
-                        //读取 String filePath = logPath + "/" + name 文件
-                        //处理，统计
-                        break;
+                        try {
+                            FileInputStream fileInputStream = new FileInputStream(filePath);
+                            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"UTF-8");
+                            BufferedReader buffReader = new BufferedReader(inputStreamReader);
+                            String line = null;
+                            
+                            //开始逐行读取文件
+                            while ((line = buffReader.readLine()) != null) {
+                                /*
+                                 * 对各个类型：
+                                 * 类型1，注释，continue操作
+                                 * 类型2，某地新增感染患者n人，读取该行文本，用split方法将其存入字符串数组
+                                 *        直接取省份字符串，用getIndex方法获取省份位置 k，然后置provinceReade数组
+                                 *        相应位置为1，获取人数后，令totalTable[k][0]和totalTable[0][0](全国)的值为相应人数
+                                 * 类型3-9与类型2大同小异，获取省份、人数，进行相关计算。
+                                 * */
+                                String textType1 = "//.*";
+                                String textType2 = "(\\S+) 新增 感染患者 (\\d+)人";
+                                String textType3 = "(\\S+) 新增 疑似患者 (\\d+)人";
+                                String textType4 = "(\\S+) 感染患者 流入 (\\S+) (\\d+)人";
+                                String textType5 = "(\\S+) 疑似患者 流入 (\\S+) (\\d+)人";
+                                String textType6 = "(\\S+) 死亡 (\\d+)人";
+                                String textType7 = "(\\S+) 治愈 (\\d+)人";
+                                String textType8 = "(\\S+) 疑似患者 确诊感染 (\\d+)人";
+                                String textType9 = "(\\S+) 排除 疑似患者 (\\d+)人";
+                                
+                                //匹配
+                                boolean isMatch1 = Pattern.matches(textType1, line);
+                                boolean isMatch2 = Pattern.matches(textType2, line);
+                                boolean isMatch3 = Pattern.matches(textType3, line);
+                                boolean isMatch4 = Pattern.matches(textType4, line);
+                                boolean isMatch5 = Pattern.matches(textType5, line);
+                                boolean isMatch6 = Pattern.matches(textType6, line);
+                                boolean isMatch7 = Pattern.matches(textType7, line);
+                                boolean isMatch8 = Pattern.matches(textType8, line);
+                                boolean isMatch9 = Pattern.matches(textType9, line);
+                                
+                                //测试输出
+                                if (isMatch1){
+                                    //是注释类型
+                                    System.out.println(line+ " | 是类型1");
+                                    continue;
+                                }
+                                if (isMatch2){
+                                    //某地新增感染人数
+                                    System.out.println(line+ " | 是类型2");
+                                }
+                                if (isMatch3){
+                                    System.out.println(line+ " | 是类型3");
+                                }
+                                if (isMatch4){
+                                    System.out.println(line+ " | 是类型4");
+                                }
+                                if (isMatch5){
+                                    System.out.println(line+ " | 是类型5");
+                                }
+                                if (isMatch6){
+                                    System.out.println(line+ " | 是类型6");
+                                }
+                                if (isMatch7){
+                                    System.out.println(line+ " | 是类型7");
+                                }
+                                if (isMatch8){
+                                    System.out.println(line+ " | 是类型8");
+                                }
+                                if (isMatch9){
+                                    System.out.println(line+ " | 是类型9");
+                                }
+                                
+                            }
+                        } catch (Exception e) {
+                            
+                        }
                     } else {
                         //指定日期比name的日期晚
                         break;
@@ -153,6 +226,7 @@ public class InfectStatistic {
         }
       
     }
+    //
     public static int getIndex(String str [],String value) {
         for (int i = 0; i < str.length; i++) {
             if (str[i].equals(value)) {
@@ -161,4 +235,5 @@ public class InfectStatistic {
         }
         return -1;
     }
+    //
 }
