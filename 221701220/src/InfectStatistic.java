240ca49e3a9979b2a91cd51726919f2d39a0a699
SilverBay <wdrt68@foxmail.com>
2020-02-17 23:49:21 +0800

    编写注释

--- 221701220/src/InfectStatistic.java
+++ 221701220/src/InfectStatistic.java
@@ -4,7 +4,7 @@
  *
  * @author WeiNan Zhao
  * @version 1.0.0
- * @since 2020.01.15
+ * @since 2020.02.15
  */
 
 
@@ -15,6 +15,11 @@ import java.util.regex.*;
 import java.io.*;
 
 
+/**
+ * 将患者类型用枚举类表示，有利于拓展和数据处理
+ * 例如将感染患者细化为轻症患者和重症患者，只需修改枚举类，并修改AbstartLogLineProcessor责任链的处理即可
+ * typename用于输出，abbr用于提取-type选项参数值
+ */
 enum PatientType {
 
     INFECTION("感染患者","ip"),
@@ -33,6 +38,10 @@ enum PatientType {
     public String getTypeName() { return typeName; }
     public String getAbbr() { return abbr; }
 
+    /**
+     * @param abbr 选项的缩写
+     * @return abbr匹配返回缩写对应枚举变量，否则返回null
+     */
     public static PatientType getTypeByAbbr(String abbr) {
         for(PatientType patientType : PatientType.values()) {
             if(abbr.equals(patientType.getAbbr())) {
@@ -45,6 +54,9 @@ enum PatientType {
 }
 
 
+/**
+ * 将list命令的选项用枚举类表示，便于拓展和数据处理
+ */
 enum Option {
 
     LOG("-log"), OUT("-out"),
@@ -58,6 +70,10 @@ enum Option {
 
     public String getOptionString() { return optionString; }
 
+    /**
+     * @param optionString 选项的文本内容
+     * @return optionString匹配返回缩写对应枚举变量，否则返回null
+     */
     public static Option getOptionByString(String optionString) {
         for(Option option : Option.values()) {
             if(optionString.equals(option.getOptionString())) {
@@ -69,6 +85,10 @@ enum Option {
 }
 
 
+/**
+ * 省份名称比较器
+ * 以常量字符串中的省份顺序来对省份名称进行排序
+ */
 class ProvinceComparator implements Comparator<String> {
 
     private static final String PROVINCE_STRING = "全国 安徽 北京 重庆 福建 "
@@ -87,6 +107,11 @@ class ProvinceComparator implements Comparator<String> {
 }
 
 
+/**
+ * 省份的实体类
+ * 类静态变量nationalNumbers数组用来记录全国人数变化
+ * 类私有成员localNumbers数组用来记录当地人数变化
+ */
 class Province {
 
     private static int[] nationalNumbers;
@@ -102,6 +127,11 @@ class Province {
         Province.nationalNumbers = nationalNumbers;
     }
 
+    /**
+     * @param patientType 患者类型，修改传入患者类型的数量
+     * @param changedNum 日志文件提取的变化人数，总为正
+     * 同步修改本省和全国的数据
+     */
     public void alterLocalNum(PatientType patientType, int changedNum) {
         localNumbers[patientType.ordinal()] += changedNum;
         nationalNumbers[patientType.ordinal()] += changedNum;
@@ -109,6 +139,11 @@ class Province {
 
 }
 
+
+/**
+ * 以省份名称为键，以Privince类实体为键组成的映射类
+ * 使用TreeMap来实现省份名称的排序，便于有序输出
+ */
 class ProvinceTreeMap {
 
     private TreeMap<String, Province> treeMap;
@@ -135,15 +170,20 @@ class ProvinceTreeMap {
 }
 
 
+/**
+ * 命令行处理类
+ * 根据传入的参数处理相应的数据
+ * 使用Getter将配置好的参数传入Province
+ */
 class CommandArgsProcessor {
 
-    private String[] args;
-    private int[] optionsIndex;
-    private String sourceDirectoryPath;
-    private String outputFilePath;
-    private String dateString;
-    private List<PatientType> patientTypes;
-    private TreeSet<String> provinceSet;
+    private String[] args;//传入命令行的参数列表
+    private int[] optionsIndex;//每个选项在命令行参数字符串数组中的索引
+    private String sourceDirectoryPath;//源文件夹路径
+    private String outputFilePath;//输出文件夹路径
+    private String dateString;//date选项指定的日期
+    private List<PatientType> patientTypes;//type选项指定的显示类型
+    private TreeSet<String> provinceSet;//province指定的输出省份
 
     CommandArgsProcessor(String[] args) {
         this.args = args;
@@ -200,6 +240,7 @@ class CommandArgsProcessor {
     private void typeOptionProcess() {
         patientTypes = new LinkedList<>();
         int index = optionsIndex[Option.TYPE.ordinal()];
+        //-type参数中指令的类型按照枚举变量的声明顺序存入types数组中，未指定的类型则为null
         if(index != 0) {
             PatientType patientType;
             for(int i = index + 1; i < args.length; i++) {
@@ -211,6 +252,7 @@ class CommandArgsProcessor {
                 }
             }
         }
+        //未指定-type命令，则获得所有种类
         else {
             patientTypes = Arrays.asList(PatientType.values());
         }
@@ -247,6 +289,13 @@ class CommandArgsProcessor {
 }
 
 
+/**
+ * 用责任链模式来处理单行文本
+ * 该类分别由IncreaseProcessor、MoveProcessor、DecreaseProcessor、ChangeProcessor继承
+ * 获取到的需求单行文本分别由以上类依次处理
+ * 若正则表达式匹配，则处理该需求文本，且不再继续向后传递需求文本
+ * 否则继续向后传递需求文本
+ */
 abstract class AbstractLogLineProcessor {
 
     public static String INCREASE = "新增.*人";
@@ -262,12 +311,14 @@ abstract class AbstractLogLineProcessor {
         AbstractLogLineProcessor.provinceTreeMap = provinceTreeMap;
     }
 
+    //将继承本抽象类的所有处理类链接成责任链
     public void setNextProcessor(AbstractLogLineProcessor nextProcessor) {
         this.nextProcessor = nextProcessor;
     }
 
     abstract protected void processLogLine(String logLine);
 
+    //需求在责任链上的选择和传递
     public void processorChoose(String logLine) {
         if(pattern.matcher(logLine).find()) {
             processLogLine(logLine);
@@ -280,6 +331,9 @@ abstract class AbstractLogLineProcessor {
 }
 
 
+/**
+ * 新增患者情况的处理类
+ */
 class IncreaseProcessor extends AbstractLogLineProcessor {
 
     IncreaseProcessor(String regex) {
@@ -305,6 +359,9 @@ class IncreaseProcessor extends AbstractLogLineProcessor {
 }
 
 
+/**
+ * 患者流动情况的处理类
+ */
 class MoveProcessor extends AbstractLogLineProcessor {
 
     MoveProcessor(String regex) {
@@ -333,6 +390,9 @@ class MoveProcessor extends AbstractLogLineProcessor {
 }
 
 
+/**
+ * 患者人数减少(被治愈或不幸死亡)的处理类
+ */
 class DecreaseProcessor extends AbstractLogLineProcessor {
 
     DecreaseProcessor(String regex) {
@@ -359,6 +419,9 @@ class DecreaseProcessor extends AbstractLogLineProcessor {
 }
 
 
+/**
+ * 患者类型变化(疑似患者确诊或者被排除)的处理类
+ */
 class ChangeProcessor extends AbstractLogLineProcessor {
 
     ChangeProcessor(String regex) {
@@ -386,10 +449,19 @@ class ChangeProcessor extends AbstractLogLineProcessor {
 }
 
 
+/**
+ * 用来处理单行需求文本的总类
+ * 使用以AbstractLogLineProcessor类实现的责任链来匹配和处理单行文本
+ * 从而避免了冗长的if else或者switch语句
+ */
 class LogLineProcessor {
 
     private AbstractLogLineProcessor processorChains;
 
+    /**
+     * @param provinceTreeMap 传入的构造参数
+     * 生成四个需求处理类，将他们链接成责任链，用于处理单行文本
+     */
     LogLineProcessor(ProvinceTreeMap provinceTreeMap) {
 
         AbstractLogLineProcessor.setProvinceTreeMap(provinceTreeMap);
@@ -414,6 +486,10 @@ class LogLineProcessor {
 }
 
 
+/**
+ * 文件比较器
+ * 以文件名称升序排序，以防日志文件处理出错
+ */
 class FileComparator implements Comparator<File> {
 
     @Override
@@ -424,20 +500,26 @@ class FileComparator implements Comparator<File> {
 }
 
 
+/**
+ * 文件处理类
+ * 根据指定的-date参数来选择要处理的日志文件
+ * 同时进行日期有效性的判断
+ * 根据命令行的选项和参数配置文件路径和格式化输出
+ */
 class FileProcessor {
 
-    private static final String SUFFIX = ".log.txt";
+    private static final String SUFFIX = ".log.txt";//日志文件后缀
     private TreeSet<File> fileTreeSet;
     private ProvinceTreeMap provinceTreeMap;
-    private LogLineProcessor logLineProcessor;
+    private LogLineProcessor logLineProcessor;//处理单行文本的类
 
-    private File sourceDirectory;
-    private FileOutputStream fileOutputStream;
-    private BufferedWriter bufferedWriter;
-    private String dateString;
-    private List<PatientType> patientTypes;
-    private Set<String> provinceSet;
-    private boolean allowNation;
+    private File sourceDirectory;//命令行传入的源文件夹参数
+    private FileOutputStream fileOutputStream;//命令行传入的目标文件路径的写入流
+    private BufferedWriter bufferedWriter;//文件输出的高级流
+    private String dateString;//命令行传入的date参数
+    private List<PatientType> patientTypes;//命令行指定的患者类型
+    private Set<String> provinceSet;//命令行指定的省份列表
+    private boolean allowNation;//是否允许输出全国的数据
 
     public FileProcessor(ProvinceTreeMap provinceTreeMap, CommandArgsProcessor commandArgsProcessor) {
 
@@ -448,6 +530,10 @@ class FileProcessor {
         argsUse(commandArgsProcessor);
     }
 
+    /**
+     * @param processor 命令行处理类
+     * 使用命令行处理类的处理数据来配置路径和格式化输出
+     */
     private void argsUse(CommandArgsProcessor processor) {
 
         sourceDirectory = new File(processor.getSourceDirectoryPath());
@@ -469,6 +555,9 @@ class FileProcessor {
 
     }
 
+    /**
+     * 根据-date的日期来选择需要处理那些文件
+     */
     private void setProcessedFiles() {
 
         if(dateString == null) {
@@ -488,6 +577,9 @@ class FileProcessor {
 
     }
 
+    /**
+     * 处理所有文件，将日志文件的每一行字符串都交给LogLineProcessor责任链来处理
+     */
     public void processFiles() {
 
         setProcessedFiles();
@@ -511,6 +603,9 @@ class FileProcessor {
 
     }
 
+    /**
+     * 根据布尔变量allowNation的值来决定是否输出全国情况
+     */
     private void outputNationData() {
 
         if(allowNation) {
@@ -530,6 +625,11 @@ class FileProcessor {
         }
     }
 
+    /**
+     * 将结果输出到指定的文件中
+     * 允许输出全国数据则先输出全国数据
+     * 然后继续输出各省数据
+     */
     public void outputResult() {
 
         outputNationData();
@@ -564,6 +664,11 @@ class FileProcessor {
 }
 
 
+/**
+ * 文件统计处理类
+ * 用于main函数中生成、配置处理类
+ * 并进行结果的输出
+ */
 class StatisticProcessor {
 
     private String[] args;
@@ -588,7 +693,9 @@ class StatisticProcessor {
     }
 }
 
-
+/**
+ * main函数，程序运行的入口
+ */
 class InfectStatistic {
     public static void main(String[] args) {
 
