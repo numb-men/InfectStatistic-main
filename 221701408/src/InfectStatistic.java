e20fc403133d296f1ab16a9101cf17774bd5ab23
maysky-blue <1768359097@qq.com>
2020-02-17 15:24:30 +0800

    思路理顺，搭好各主类框架

--- 221701408/src/InfectStatistic.java
+++ 221701408/src/InfectStatistic.java
@@ -2,12 +2,34 @@
  * InfectStatistic
  * TODO
  *
- * @author xxx
- * @version xxx
- * @since xxx
+ * @author 陈怡
+ * @version 1.8
+ * @since 2020.2.15
+ */
+
+/**
+ *
+ *主函数，用作测试及java命令行命令
  */
 class InfectStatistic {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld1");
     }
+}
+
+/**
+ *
+ *命令处理类
+ */
+class CmdHandle {
+
+}
+
+/**
+ *
+ * 文件处理类
+ *
+ */
+class FileHandle {
+
 }
