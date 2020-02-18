c1b0131ad8f97ec4374cdb70372a892ffadd423b
yanhar <30743657+yanhar@users.noreply.github.com>
2020-02-18 10:42:37 +0800

    获取所有文件名

--- 221701105/src/InfectStatistic.java
+++ 221701105/src/InfectStatistic.java
@@ -6,13 +6,30 @@
  * @version 0.1
  * @since xxx
  */
+import java.io.File;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 class InfectStatistic {
     public static void main(String[] args) {
         argparse arg=new argparse(args);
+        TreeSet<String> logpath=getfile();
 
+    }
+    private static TreeSet<String> getfile(){
+        TreeSet<String> files=new TreeSet<String>();
+        File logpath =new File("log");
+        File[] teplist= logpath.listFiles();
+        for (int i=0;i<teplist.length;i++){
+            if(teplist[i].isFile())
+                files.add(teplist[i].toString());
+        }
+        /*files.add("log\\2020-01-25.log.txt");
+        Iterator i=files.iterator();
+        while(i.hasNext())
+            System.out.println(i.next());*/
+        return files;
     }
 }
 
@@ -45,10 +62,12 @@ class argparse{
                         while(i+1<args.length && !args[i+1].startsWith("-")){
                             types.add(args[++i]);
                         }
+                        break;
                     case "-province":
                         while(i+1<args.length && !args[i+1].startsWith("-")){
                             provinces.add(args[++i]);
                         }
+                        break;
                 }
             }
         }
