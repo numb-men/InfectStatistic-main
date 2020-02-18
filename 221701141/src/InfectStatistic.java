206ea5f5892ac67bc40a87f66e6aae5ca9050081
massizhi <2750201946@qq.com>
2020-02-18 14:16:03 +0800

    solve no province no type

--- 221701141/src/InfectStatistic.java
+++ 221701141/src/InfectStatistic.java
@@ -1,5 +1,10 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -19,8 +24,8 @@ class InfectStatistic
     			"江西","辽宁","内蒙古","宁夏","青海","山东","山西","陕西","上海",
     			"四川","天津","西藏","新疆","云南","浙江"};
     	String type[]={"ip","sp","cure","dead"};
-    	//记录各省（包括全国）各种类型的人的数量
-    	int[][] number=new int[32][4];
+    	//记录各省（包括全国）各种类型的人的数量,最后一位记录该省是否被涉及到
+    	int[][] number=new int[32][5];
     	//建立存放各类型命令行参数的ArrayList
     	String logList=new String();
 		String outList=new String();
@@ -33,7 +38,7 @@ class InfectStatistic
 		//初始化二维数组number
     	for (int i=0;i<32;i++)
     	{
-    		for (int j=0;j<4;j++)
+    		for (int j=0;j<5;j++)
     		{
     			number[i][j]=0;
     		}
@@ -49,6 +54,7 @@ class InfectStatistic
     		}
     		else if (args[i].equals("-out")) {
     			outList=args[++i];
+    			outList=outList.replace('/', '\\');
     		}
     		else if (args[i].equals("-date")) {
     			dateList=args[++i];
@@ -107,7 +113,7 @@ class InfectStatistic
     	File file=new File(logList);
         //获取该路径下的文件和文件夹
         String[] arr=file.list();
-        //遍历
+        //处理日志中的数据
         for(String s:arr)
         {
         	if (s.compareTo(dateList+".log")>0)
@@ -137,6 +143,8 @@ class InfectStatistic
 		            		}
 		            	}
 		            	//System.out.println(index);
+		            	number[0][4]=1;
+		            	number[index][4]=1;
 		            	String second=sc.next();
 		                if (second.equals("新增")) 
 		                {
@@ -173,6 +181,7 @@ class InfectStatistic
 			            	}
 		                	number[index][0]-=member;
 		                	number[index1][0]+=member;
+		                	number[index1][4]=1;
 		                }
 		                else if (second.equals("疑似患者"))
 		                {
@@ -194,6 +203,7 @@ class InfectStatistic
 				            	}
 			                	number[index][1]-=member;
 			                	number[index1][1]+=member;
+			                	number[index1][4]=1;
 		                	}
 		                	else
 		                	{
@@ -244,7 +254,56 @@ class InfectStatistic
 				e.printStackTrace();
 			}           
         }
-    	
+        
+    	//得到输出结果
+        ArrayList<String> result=new ArrayList<String>();
+        if (provinceInt.size()==0)
+        {
+        	if (typeInt.size()==0)
+        	{
+        		for (int i=0;i<32;i++)
+            	{
+        			if (number[i][4]==0)
+        			{
+        				continue;
+        			}
+        			String mid=province[i]+" 感染患者"+""+number[i][0]+""+"人"+" 疑似患者"+""+number[i][1]+""+"人"
+        			+" 治愈"+""+number[i][2]+""+"人"+" 死亡"+""+number[i][3]+""+"人\n";
+        			result.add(mid);
+            	}
+        	}
+        	else
+        	{
+        		
+        	}
+        }
+        else
+        {
+        	if(typeInt.size()==0)
+        	{
+        		
+        	}
+        	else
+        	{
+        		
+        	}
+        }
+        
+        //输出结果到指定文件中
+        try {
+	        FileOutputStream out = new FileOutputStream(outList);
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+	        for (int i=0;i<result.size();i++) {
+	        	bw.write(result.get(i));
+	        }
+	        bw.write("// 该文档并非真实数据，仅供测试使用\n");
+	        bw.close();
+        }
+        catch (Exception e) 
+		{
+			e.printStackTrace();
+		}   
+        
     	//test
     	System.out.println(logList);
     	System.out.println(outList);
@@ -255,7 +314,6 @@ class InfectStatistic
     	for (int i=0;i<provinceInt.size();i++)
     		System.out.print(provinceInt.get(i));
     	System.out.println();
-    	//list -date 2020-01-22 -province 江苏 福建 -log D:/log/ -out D:/output.txt -type ip sp cure
     	
     	for (int i=0;i<32;i++)
     	{
