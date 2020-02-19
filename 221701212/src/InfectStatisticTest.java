aac4bc2024903da10342c4b5f268d34f565a1bcf
maoyudun <598360870@qq.com>
2020-02-20 00:55:49 +0800

    删除多余文件

--- 221701212/src/InfectStatisticTest.java
+++ 221701212/src/InfectStatisticTest.java
@@ -6,44 +6,47 @@ import org.junit.Test;
 public class InfectStatisticTest extends TestCase {
     @Test
     public void testInfectStatistic1() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout1.txt"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output1.txt"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic2() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout2.txt","-date","2020-02-02"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output2.txt","-date","2020-01-22"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic3() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout3.txt","-date","2020-01-27"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output3.txt","-date","2020-01-23"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic4() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout4.txt","-date","2020-01-07"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output4.txt","-date","2020-01-27"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic5() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout5.txt","-type","ip","-date","2020-02-27"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output5.txt","-date","2020-01-27","-type","sp"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic6() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout6.txt","-type","si","sp","-date","2020-02-27"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output6.txt","-date","2020-01-27","-type","sp","ip"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic7() {
-        String[] str = {"list","-province","湖北","-log","D:\\log","-out","D:\\log\\testout7.txt","-date","2020-02-27"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output7.txt","-date","2020-01-27",
+                "-province","湖北"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic8() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout8.txt","-province","江苏","全国"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output8.txt","-type","ip","sp",
+                "-province","全国","福建"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic9() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout9.txt","-date","2020-02-01",
-                "-province","全国","江西"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output9.txt","-date","2020-01-27",
+                "-province","全国","浙江"};
         InfectStatistic.main(str);
     }
     public void testInfectStatistic10() {
-        String[] str = {"list","-log","D:\\log","-out","D:\\log\\testout10.txt","-date","2020-03-27"};
+        String[] str = {"list","-log","D:\\log","-out","D:\\out\\output10.txt","-date","2020-01-22",
+                "-province","全国","浙江","安徽","湖北","-type","dead","cure","sp","ip"};
         InfectStatistic.main(str);
     }
 }
