9ffb77459085b0d1b69f7ac6794e92e819af6226
numb-men <le@hengyumo.cn>
2020-03-31 10:46:46 +0800

    add merge.bat

---
+++ merge.bat
@@ -0,0 +1,9 @@
+set pr_name=ANDTENET
+git checkout -b %pr_name%-master master
+git pull https://github.com/%pr_name%/InfectStatistic-main.git master
+del .gitignore
+git add .
+git commit -m"merge conflicts"
+git checkout master
+git merge --no-ff %pr_name%-master
+git push origin master
